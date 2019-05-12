b650eeaecb3ccbd9ffc02b0b422cd47b7cccff1b
joonyboy <seojoonwon@naver.com>
2019-05-12 23:28:48 +0900

    Bolt의 Embed State Machine을 이용하여 Turn 시스템을 만든뒤 전투 페이즈에 Custom Script(BattleSimulator.cs)의 특정함수(StartBattleSimulation)를 실행하는 테스트를 함,

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,18 +1,23 @@
-﻿using System.Collections;
+﻿using Bolt;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
+    [SerializeField] GameObject phaseManager;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    IEnumerator Start() {
+        yield return new WaitForSeconds(3.0f);
+        CustomEvent.Trigger(phaseManager, "EndTurn");
+        yield return new WaitForSeconds(3.0f);
+        CustomEvent.Trigger(phaseManager, "EndTurn");
+        yield return new WaitForSeconds(3.0f);
+        CustomEvent.Trigger(phaseManager, "EndTurn");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
