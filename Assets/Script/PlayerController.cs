a7deb9cdb9240f50cdaa34fe9ad79de1a3b217b7
Kwak Yong Ju <amese1225@fbl.kr>
2019-05-13 12:00:07 +0900

    컨플릭트 부분 제거

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,5 +1,4 @@
-﻿using Bolt;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,20 +7,10 @@ public class PlayerController : MonoBehaviour
     public bool race;
     public int[,] placement = new int[2, 5] { {0,0,0,0,0 },
                                               {0,0,0,0,0 }};
-
-
     public GameObject card;
 
 
-    // Start is called before the first frame update
-    IEnumerator Start() {
-        yield return new WaitForSeconds(3.0f);
-        CustomEvent.Trigger(phaseManager, "EndTurn");
-        yield return new WaitForSeconds(3.0f);
-        CustomEvent.Trigger(phaseManager, "EndTurn");
-        yield return new WaitForSeconds(3.0f);
-        CustomEvent.Trigger(phaseManager, "EndTurn");
-    }
+    
 
     // Update is called once per frame
     void Update()
