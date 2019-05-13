004c91b694544eb28c860dfd930c6b0fc4de47d0
joonyboy <seojoonwon@naver.com>
2019-05-13 15:51:47 +0900

    컬랙션 버튼을 클릭하면 등장하는 덱 세팅 씬 추가. 덱 세팅 씬 및 메뉴 씬 Header를 공통적으로 갖고 있어 따로 불러오는 방식으로 변경함. Header에 있는 버튼들 처리를 위한 HeaderController 추가.

---
+++ Assets/Script/MainMenu/Controllers/HeaderController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeaderController : MonoBehaviour {
+    [SerializeField] GameObject ShopPanel, OptionPanel, MyFriendPanel, NotifyPanel;
+    // Start is called before the first frame update
+    void Start() {
+        SetMyResource();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void OnOptionBtnClicked() {
+
+    }
+
+    public void OnFriendBtnClicked() {
+
+    }
+
+    public void OnShopBtnClicked() {
+
+    }
+
+    public void SetMyResource() {
+
+    }
+}
