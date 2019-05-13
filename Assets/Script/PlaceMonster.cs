dd9bff198e0978dd1fe9451fec4cc05b4d6d1dc3
Kwak Yong Ju <amese1225@fbl.kr>
2019-05-13 11:54:39 +0900

    몬스터 작업중, 몬스터의 스탯을 위한 클래스 추가, 플레이어 컨트롤러 각 오브젝트에 배치

---
+++ Assets/Script/PlaceMonster.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Monster;
+
+
+public class PlaceMonster : MonoBehaviour
+{
+    public Unit unit;
+
+    private void Start()
+    {
+        unit.cost = 0;
+        unit.HP = 1;
+    }
+
+
+}
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public bool race;
+    public int[,] placement = new int[2, 5] { {0,0,0,0,0 },
+                                              {0,0,0,0,0 }};
+
+
+    public GameObject card;
+
+
     // Start is called before the first frame update
     void Start()
     {
