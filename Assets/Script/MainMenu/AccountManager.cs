062319a50931e0ac2de7dbd26be9aaa333d12e25
joonyboy <seojoonwon@naver.com>
2019-05-13 13:44:12 +0900

    기본 NetworkManager 프레임 추가, SingleTon 라이브러리 추가. AccountManager 싱글턴으로 전환

--- Assets/Script/MainMenu/AccountManager.cs
+++ Assets/Script/MainMenu/AccountManager.cs
@@ -1,18 +1,19 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
-public class AccountManager : MonoBehaviour
-{
+public class AccountManager : Singleton<AccountManager> {
+    protected AccountManager() { }
+    public string DEVICEID { get; private set; }
+    private StringBuilder sb = new StringBuilder();
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
     }
 }
