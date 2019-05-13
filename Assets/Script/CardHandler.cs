91c263f9e1a5d8f0aefc591b6df5bd5b6904e137
KHG <cow3323@fbl.kr>
2019-05-13 15:34:52 +0900

    카드 정보 ScrollList 팝업 작업중

--- Assets/Script/CardHandler.cs
+++ Assets/Script/CardHandler.cs
@@ -1,15 +1,18 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.UI.Extensions;
 
 public class CardHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public Vector3 startPos;
+    private bool blockButton = false;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
+        blockButton = true;
         startPos = transform.position;
     }
 
@@ -20,11 +23,16 @@ public class CardHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        if(Vector3.Distance(transform.position, startPos) < 1.0f){
-
-        }
         transform.position = startPos;
+        blockButton = false;
     }
 
-
+    public void OpenCardInfoList() {
+        if (!blockButton) {
+            Transform css = GameObject.Find("Canvas").transform.GetChild(2);
+            css.gameObject.SetActive(true);
+            css.GetComponentInChildren<HorizontalScrollSnap>().GoToScreen(transform.GetSiblingIndex());
+            
+        }
+    }
 }
