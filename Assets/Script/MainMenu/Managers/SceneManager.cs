78bb26846534df4a4586f30f4d7a3a39b4429562
joonyboy <seojoonwon@naver.com>
2019-05-14 15:27:36 +0900

    PVP 대전화면 초기 레이아웃 구성완료. 모바일 기기의 뒤로가기 클릭 제어를 위한 코드 작성중

--- Assets/Script/MainMenu/Managers/SceneManager.cs
+++ Assets/Script/MainMenu/Managers/SceneManager.cs
@@ -4,6 +4,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SceneManager : Singleton<SceneManager> {
+    private float secondsToLoadNextScene = 0.5f;
+    private static int lastScene;
+    private int mainScene = 1;
+    private int currentScene;
+
+    public static Stack<int> sceneStack = new Stack<int>();
+
+    private void Awake() {
+        currentScene = UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex;
+    }
+
     // Start is called before the first frame update
     void Start() {
         DontDestroyOnLoad(this);
@@ -11,44 +22,94 @@ public class SceneManager : Singleton<SceneManager> {
 
     // Update is called once per frame
     void Update() {
-
+        BackButtonPressed();
     }
 
     public void LoadScene(Scene scene) {
-        string str = null;
+        int numberOfScene = -1;
         switch (scene) {
             case Scene.MAIN_SCENE:
-                str = "MenuScene";
+                numberOfScene = 0;
                 break;
             case Scene.MISSION_SELECT_SCENE:
-                str = "MissionSelectScene";
+                numberOfScene = 1;
                 break;
             case Scene.COLLECTION_SCENE:
-                str = "CollectionScene";
+                numberOfScene = 6;
                 break;
             case Scene.MISSION_INGAME:
-                str = "IngameScene";
+                numberOfScene = 2;
+                break;
+            case Scene.PVP_READY_SCENE:
+                numberOfScene = 4;
+                break;
+            case Scene.DECK_SETTING_SCENE:
+                numberOfScene = 5;
                 break;
         }
-        if(str != null) {
+
+        if(numberOfScene != -1) {
             Fader fader = GameObject.Find("FaderCanvas/Fader").GetComponent<Fader>();
             if(fader == null) {
-                UnityEngine.SceneManagement.SceneManager.LoadScene(str);
+                //UnityEngine.SceneManagement.SceneManager.LoadScene(str);
+                LoadNextScene(numberOfScene);
             }
             else {
                 fader.StartFade(Fader.FadeDirection.In, delegate {
-                    UnityEngine.SceneManagement.SceneManager.LoadScene(str);
+                    LoadNextScene(numberOfScene);
                 });
             }
         }
+    }
 
+    public void LoadNextScene(int numberOfSceneToLoad) {
+        StartCoroutine(LoadScene(numberOfSceneToLoad));
+    }
 
+    private IEnumerator LoadScene(int numberOfScene) {
+        SetLastScene(currentScene);
+
+        yield return new WaitForSeconds(secondsToLoadNextScene);
+        LoadNewScene(numberOfScene);
+    }
+
+    public void BackButtonPressed() {
+        if (Input.GetKey(KeyCode.Escape) && currentScene > mainScene) {
+            if (lastScene == 0) {
+                Debug.Log("Last scene was Splash Screen so load Main Scene instead.");
+                UnityEngine.SceneManagement.SceneManager.LoadScene(mainScene);
+            }
+            else {
+                LoadLastScene();
+            }
+        }
+    }
+
+    public void LoadNewScene(int sceneToLoad) {
+        currentScene = UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex;
+        sceneStack.Push(currentScene);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void LoadLastScene() {
+        lastScene = sceneStack.Pop();
+        UnityEngine.SceneManagement.SceneManager.LoadScene(lastScene);
+    }
+
+    public static void SetLastScene(int makeCurrentSceneTheLastScene) {
+        lastScene = makeCurrentSceneTheLastScene;
+    }
+
+    public static int GetLastScene() {
+        return lastScene;
     }
 
     public enum Scene {
         MAIN_SCENE,
         COLLECTION_SCENE,
         MISSION_SELECT_SCENE,
-        MISSION_INGAME
+        MISSION_INGAME,
+        PVP_READY_SCENE,
+        DECK_SETTING_SCENE
     }
 }
--- Assets/Script/MainMenu/MenuSceneController.cs
+++ Assets/Script/MainMenu/MenuSceneController.cs
@@ -13,7 +13,17 @@ public class MenuSceneController : MonoBehaviour {
 
     }
 
+    /// <summary>
+    /// 싱글플레이 미션 버튼 클릭 (식물, 좀비)
+    /// </summary>
     public void OnMissionClicked() {
         SceneManager.Instance.LoadScene(SceneManager.Scene.MISSION_SELECT_SCENE);
+    }
+
+    /// <summary>
+    /// PVP대전 버튼 클릭
+    /// </summary>
+    public void OnPVPClicked() {
+        SceneManager.Instance.LoadScene(SceneManager.Scene.PVP_READY_SCENE);
     }
 }
