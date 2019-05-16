4f4f00f67bee337998cf7466e4d9f9d08ff6cb25
KHG <cow3323@fbl.kr>
2019-05-16 16:31:15 +0900

    카드 데이터 패키지 Script 오류 해결

--- Assets/Script/CardData.cs
+++ Assets/Script/CardData.cs
@@ -1,14 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System;
-
-[CreateAssetMenu]
-public class CardDataPackage : ScriptableObject{
-    public string ClientVersion;
-    public Dictionary<string, CardData> data = new Dictionary<string, CardData>();
-}
-
 
 public class CardData {
     //public new string id;
