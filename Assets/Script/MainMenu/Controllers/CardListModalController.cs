37b543b4ee1035a3023f116c0447bcf42832bde1
joonyboy <seojoonwon@naver.com>
2019-05-16 22:04:46 +0900

    덱 클릭시 등장하는 카드리스트 모달 레이아웃 구성 완료. PVP_ReadyScene에 비활성화 상태로 추가

---
+++ Assets/Script/MainMenu/Controllers/CardListModalController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardListModalController : MonoBehaviour {
+    
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void Init(List<dataModules.CardInventory> cards) {
+
+    }
+
+    private void CreateUnlockedCardList() {
+
+    }
+
+    private void CreateLockedCardList() {
+
+    }
+}
