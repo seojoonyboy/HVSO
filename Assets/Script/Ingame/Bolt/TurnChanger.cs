2a40760b2943a80d4b4a0b90b25385702069e502
joonyboy <seojoonwon@naver.com>
2019-05-16 14:53:07 +0900

    PhaseMachine 누락된 데이터 추가.

--- Assets/Script/Ingame/Bolt/TurnChanger.cs
+++ Assets/Script/Ingame/Bolt/TurnChanger.cs
@@ -38,6 +38,7 @@ public class TurnChanger : MonoBehaviour {
     IEnumerator TestNextTurn() {
         yield return new WaitForSeconds(3.0f);
         CustomEvent.Trigger(gameObject, "EndTurn");
+        //Debug.Log("턴 종료");
     }
 
     public enum TurnType {
