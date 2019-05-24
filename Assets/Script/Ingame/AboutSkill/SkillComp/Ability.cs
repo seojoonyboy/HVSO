c0a06f3e7556aa1dadaf15877bb251ec4e66b388
joonyboy <seojoonwon@naver.com>
2019-05-24 10:33:07 +0900

    기존 skill_id로 카드 스킬 관련 스크립트 명명하는 방식에서 skill method이름으로 명명하는 방식으로 변경. 그에 따라 cs100026 이라는 이름의 스킬 스크립트를 footslog로 스크립트 이름 변경

---
+++ Assets/Script/Ingame/AboutSkill/SkillComp/Ability.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// 카드 고유 능력 처리에 대한 스크립트
+/// </summary>
+public class Ability : MonoBehaviour {
+    protected ActivatedSkillsManager skillManager;
+    public UnityEvent<object> OnDropFinished;
+    public UnityEvent<object> OnBeginDragFinished;
+
+    void Awake() {
+        skillManager = ActivatedSkillsManager.Instance;    
+    }
+
+    public virtual void InitData(object data) { }
+
+    protected virtual void BeginCardPlay() { }
+    protected virtual void EndCardPlay() { }
+    protected virtual void OnAttack() { }
+
+    public virtual IEnumerator OnBeginDrag() {
+        yield return null;
+    }
+}
