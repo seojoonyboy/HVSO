3aeb685052b17edee896d68a604ab56646dd19a4
KHG <cow3323@fbl.kr>
2019-05-24 18:51:52 +0900

    카드 초상화 시리얼라이즈 모노스크립트로 리소스화, 카드에 적용.

---
+++ Assets/Script/ResourceManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using Sirenix.OdinInspector;
+
+[Sirenix.OdinInspector.ShowOdinSerializedPropertiesInInspector]
+public class ResourceManager : SerializedMonoBehaviour
+{
+    public Dictionary<string, Sprite> cardPortraite;
+}
