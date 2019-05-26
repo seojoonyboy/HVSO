48439e218d2071f655ed2361d9ed3cc625f52d11
joonyboy <seojoonwon@naver.com>
2019-05-26 19:56:09 +0900

    버프(gain) 스킬 구현중 중간 커밋

--- Assets/Script/Ingame/AboutSkill/SkillComp/Ability_gain.cs
+++ Assets/Script/Ingame/AboutSkill/SkillComp/Ability_gain.cs
@@ -6,6 +6,13 @@ namespace SkillModules {
     public class Ability_gain : Ability {
         public override void EndCardPlay() {
             Debug.Log("버프를 줄 대상을 골라주세요.");
+
+            PlaceMonster[] units = FindObjectsOfType<PlaceMonster>();
+            foreach(PlaceMonster monster in units) {
+                if(monster.isPlayer && monster.gameObject != gameObject) {
+                    //monster.transform.Find("ClickableUI").gameObject.SetActive(true);
+                }
+            }
         }
     }
 }
