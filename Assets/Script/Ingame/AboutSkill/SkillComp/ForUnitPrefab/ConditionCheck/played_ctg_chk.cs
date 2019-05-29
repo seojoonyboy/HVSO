30b539b224da94df16799c90208b15b086edb95d
joonyboy <seojoonwon@naver.com>
2019-05-29 18:50:32 +0900

    소환된 유닛의 카테고리 비교 처리 완료.

--- Assets/Script/Ingame/AboutSkill/SkillComp/ForUnitPrefab/ConditionCheck/played_ctg_chk.cs
+++ Assets/Script/Ingame/AboutSkill/SkillComp/ForUnitPrefab/ConditionCheck/played_ctg_chk.cs
@@ -8,10 +8,10 @@ namespace SkillModules {
         public override bool IsConditionSatisfied(bool isPlayerUnitGenerated, GameObject summonedObj) {
             bool result = false;
             PlaceMonster placeMonster = summonedObj.GetComponent<PlaceMonster>();
-
+            string[] cardCategories = placeMonster.unit.cardCategories;
             foreach (Condition condition in data.conditions) {
-                if (condition.method == "army") {
-                    //placeMonster.unit.
+                foreach(string ctg in cardCategories) {
+                    if (condition.method == ctg) result = true;
                 }
             }
             return result;
