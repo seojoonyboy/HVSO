4c8f5e312275ac77fc2f87362c4576654a2c2702
joonyboy <seojoonwon@naver.com>
2019-05-31 18:49:01 +0900

    stun 마법 구현중. 카드 수급 마법 구현중

--- Assets/Script/Ingame/AboutSkill/SkillComp/Ability_supply.cs
+++ Assets/Script/Ingame/AboutSkill/SkillComp/Ability_supply.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
-public class Ability_supply : Ability {
-    // Start is called before the first frame update
-    void Start() {
+namespace SkillModules {
+    public class Ability_supply : Ability {
+        protected override void OnEventCallback(object parm) {
+            var effectData = skillData.effects.ToList().Find(x => x.method == "supply");
+            int drawNum = 0;
 
-    }
-
-    // Update is called once per frame
-    void Update() {
-
+            int.TryParse(effectData.args[0], out drawNum);
+            for(int i=0; i<drawNum; i++) {
+                PlayMangement.instance.player.cdpm.AddCard();
+            }
+        }
     }
 }
