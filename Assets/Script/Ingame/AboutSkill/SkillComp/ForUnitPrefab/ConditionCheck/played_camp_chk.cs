73b9f53574d8bc161a92e3abc5fcaf95fc7e724e
joonyboy <seojoonwon@naver.com>
2019-06-05 12:47:57 +0900

    버그 수정

--- Assets/Script/Ingame/AboutSkill/SkillComp/ForUnitPrefab/ConditionCheck/played_camp_chk.cs
+++ Assets/Script/Ingame/AboutSkill/SkillComp/ForUnitPrefab/ConditionCheck/played_camp_chk.cs
@@ -31,7 +31,13 @@ namespace SkillModules {
                     else if(arg == "enemy") {
                         //아군 마법 카드 사용시
                         //아군 유닛 소환시
-                        if(summonedObj.GetComponent<PlaceMonster>() != null) {
+                        if (summonedObj.GetComponent<PlaceMonster>() == null) {
+                            if (gameObject.GetComponent<PlaceMonster>().isPlayer == false) {
+                                isCampValid = true;
+                            }
+                        }
+
+                        if (summonedObj.GetComponent<PlaceMonster>() != null) {
                             //내 자신의 isPlayer와 비교
                             if (gameObject.GetComponent<PlaceMonster>().isPlayer != summonedObj.GetComponent<PlaceMonster>().isPlayer) {
                                 isCampValid = true;
