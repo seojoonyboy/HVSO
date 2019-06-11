9b34c63cf12dfa57af2076f0f1af69563f809ca2
joonyboy <seojoonwon@naver.com>
2019-06-11 14:30:10 +0900

    gain Ability 초안 작성.

--- Assets/Script/Ingame/AboutSkill/SkillComp/0611Renewal/Ability.cs
+++ Assets/Script/Ingame/AboutSkill/SkillComp/0611Renewal/Ability.cs
@@ -5,11 +5,7 @@ using SkillModules;
 using System.Linq;
 
 namespace SkillModules {
-    public class Ability : MonoBehaviour {
-        protected bool IsAllConditionsSatisfied() {
-            List<ConditionChecker> checkers = GetComponents<ConditionChecker>().ToList();
-            if (checkers.Exists(x => x.IsConditionSatisfied() == false)) return false;
-            return true;
-        }
+    public partial class Ability : MonoBehaviour {
+
     }
 }
