aa4d44ff0a354467e739a5560bf56eb429f6f54a
joonyboy <seojoonwon@naver.com>
2019-06-21 12:20:31 +0900

    스턴 걸렸을 때 텍스트 추가

--- Assets/Script/Ingame/AboutSkill/SkillComp/0611Renewal/stun.cs
+++ Assets/Script/Ingame/AboutSkill/SkillComp/0611Renewal/stun.cs
@@ -1,7 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 namespace SkillModules {
-    public class stun : MonoBehaviour { }
+    public class stun : MonoBehaviour {
+        private TextMeshPro textPro;
+        private void Start() {
+            TextMeshPro textPro = transform.Find("Status").GetComponent<TextMeshPro>();
+            textPro.gameObject.SetActive(true);
+            textPro.text = "스턴";
+        }
+    }
 }
