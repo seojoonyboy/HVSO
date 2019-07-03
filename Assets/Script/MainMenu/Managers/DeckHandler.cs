3eb93608425c888c3493d64081d5ccfb84b0b0c9
KHG <cow3323@fbl.kr>
2019-07-03 20:15:11 +0900

    덱 세팅 메뉴 설정중

---
+++ Assets/Script/MainMenu/Managers/DeckHandler.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckHandler : MonoBehaviour
+{
+    private int deckID;
+
+    public int DECKID {
+        get { return deckID; }
+        set { deckID = value; }
+    }
+}
--- Assets/Script/MainMenu/Managers/DeckSettingManager.cs
+++ Assets/Script/MainMenu/Managers/DeckSettingManager.cs
@@ -1,18 +1,10 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DeckSettingManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public void ClickNewDeck() {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
