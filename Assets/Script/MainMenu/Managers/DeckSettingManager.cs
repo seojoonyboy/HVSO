09aa99002a02ce992122c253c7107fa52c91ce01
KHG <cow3323@fbl.kr>
2019-07-04 10:47:15 +0900

    덱 편집메뉴 작업중

--- Assets/Script/MainMenu/Managers/DeckSettingManager.cs
+++ Assets/Script/MainMenu/Managers/DeckSettingManager.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class DeckSettingManager : MonoBehaviour
 {
-    public void ClickNewDeck() {
-
+    [SerializeField] Canvas humanTemplateCanvas;
+    [SerializeField] Canvas orcTemplateCanvas;
+    public void ClickNewDeck(DeckHandler deck) {
+        //deck.DECKID
+        if (deck.gameObject.name == "HumanEditDeck")
+            humanTemplateCanvas.gameObject.SetActive(true);
+        else
+            orcTemplateCanvas.gameObject.SetActive(true);
     }
 }
