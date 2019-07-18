c64d8f266c3d055c7af6fa2deb89581ca973fbf2
joonyboy <seojoonwon@naver.com>
2019-07-18 10:32:40 +0900

    이전 UnityWebRequest 관련 코드 모두 제거

--- Assets/Script/NetworkManager.cs
+++ Assets/Script/NetworkManager.cs
@@ -6,119 +6,23 @@ using UnityEngine.Networking;
 using System.Collections.Generic;
 using UnityEngine.UI;
 
-public partial class NetworkManager : Singleton<NetworkManager> {
-    #if UNITY_EDITOR
+/// <summary>
+/// BestHTTP Pro
+/// </summary>
+public partial class NetworkManager : MonoBehaviour {
+#if UNITY_EDITOR
     private string url = "https://ccdevclient.fbl.kr/";
-    #else
+#else
     private string url = "https://cctest.fbl.kr/";
-    #endif
-    public string baseUrl {get {return url;}}
+#endif
+    public string baseUrl { get { return url; } }
     protected NetworkManager() { }
-    public delegate void Callback(HttpResponse response);
-    public delegate void CallbackRetryOccured(string msg);
 
     private void Awake() {
         DontDestroyOnLoad(gameObject);
         MAX_REDIRECTCOUNT = 10;
     }
 
-    public void request(string method, string url, string data, Callback callback, bool neeAuthor = true) {
-        StartCoroutine(_request(method, url, data, callback, neeAuthor));
-    }
-
-    //overload
-    IEnumerator _request(string method, string url, string data, Callback callback, bool needAuthor = true) {
-        UnityWebRequest _www;
-        switch (method) {
-            case "POST":
-                _www = UnityWebRequest.Post(url, data);
-                break;
-            case "PUT":
-                _www = UnityWebRequest.Put(url, data);
-                _www.method = "POST";
-                _www.SetRequestHeader("Content-Type", "application/json");
-                break;
-            case "DELETE":
-                _www = UnityWebRequest.Delete(url);
-                break;
-            case "GET":
-            default:
-                _www = UnityWebRequest.Get(url);
-                break;
-        }
-        _www.timeout = 10;
-
-        if (needAuthor) {
-            //_www.SetRequestHeader("Authorization", "Token " + GameManager.Instance.userStore.userTokenId);
-            //_www.downloadHandler = new DownloadHandlerBuffer();
-        }
-
-        using (UnityWebRequest www = _www) {
-            yield return www.SendWebRequest();
-            callback(new HttpResponse(www));
-        }
-    }
-}
-
-public partial class NetworkManager {
-    public GameObject subReqPrefab;
-    //TODO : Queue(FIFO)의 형태로 Network 요청을 쌓아 처리하도록 수정
-    Queue networkQueue = new Queue();
-
-    //overload
-    /// <summary>
-    /// 다중 요청 시도
-    /// </summary>
-    /// <param name="method">POST/GET/PUT/DELETE</param>
-    /// <param name="url">URL</param>
-    /// <param name="data"></param>
-    /// <param name="callback"></param>
-    /// <param name="needAuthor"></param>
-    /// <param name="tryCount"></param>
-    /// <returns></returns>
-    public void request(string method, string url, Callback callback, CallbackRetryOccured retryMessageCallback, bool needAuthor = true, int tryCount = 3) {
-        WWWForm data = null;
-
-        GameObject subReqObj = Instantiate(subReqPrefab, transform);
-        NetworkModules.Request subReq = subReqObj.GetComponent<NetworkModules.Request>();
-
-        subReq.StartRequest(
-            method: method,
-            url: url,
-            data: data,
-            tryCount: tryCount,
-            callback: callback,
-            retryMessageCallback: retryMessageCallback);
-    }
-}
-
-public class HttpResponse {
-    public bool isError;
-    public string errorMessage;
-    public string data;
-    public long responseCode;
-    public UnityWebRequest request;
-    public string header;
-    public HttpResponse(UnityWebRequest _request){
-        request = _request;
-        responseCode = _request.responseCode;
-        isError = _request.isNetworkError;
-        errorMessage = _request.error;
-        if(_request.downloadHandler == null) {
-            data = null;
-        }
-        else {
-            data = _request.downloadHandler.text;
-        }
-        header = _request.GetResponseHeader("Link");
-    }
-}
-
-/// <summary>
-/// BestHTTP Pro
-/// 점진적으로 기존 HTTP 요청을 모두 BestHTTP로 통합/변경 예정
-/// </summary>
-public partial class NetworkManager {
     Queue<RequestFormat> requests = new Queue<RequestFormat>();
     private bool dequeueing = false;
     TimeSpan timeout = new TimeSpan(0, 0, 30);  //timeout 30초 지정
@@ -129,6 +33,15 @@ public partial class NetworkManager {
         if (dequeueing) return;
         if (requests.Count == 0) return;
         DequeueRequest();
+    }
+
+    /// <summary>
+    /// HTTP 요청
+    /// </summary>
+    /// <param name="request">HTTPRequest에 맞는 Format 작성</param>
+    /// <param name="callback">요청 완료시 받을 Callback</param>
+    public void Request(HTTPRequest request, OnRequestFinishedDelegate callback, string msg = null) {
+        requests.Enqueue(new RequestFormat(request, callback, msg));
     }
 
     private void DequeueRequest() {
@@ -190,15 +103,6 @@ public partial class NetworkManager {
         dequeueing = false;
         Destroy(loadingModal);
         loadingModal = null;
-    }
-
-    /// <summary>
-    /// HTTP 요청
-    /// </summary>
-    /// <param name="request">HTTPRequest에 맞는 Format 작성</param>
-    /// <param name="callback">요청 완료시 받을 Callback</param>
-    public void Request(HTTPRequest request, OnRequestFinishedDelegate callback, string msg = null) {
-        requests.Enqueue(new RequestFormat(request, callback, msg));
     }
 
     public class RequestFormat {
