ef373f95c4280c264731d611134072d684606582
KHG <cow3323@fbl.kr>
2019-07-30 18:11:27 +0900

    덱 9개까지 생성 가능, 덱 리스트 페이지 구현

--- Assets/Script/MainMenu/Managers/DeckSettingManager.cs
+++ Assets/Script/MainMenu/Managers/DeckSettingManager.cs
@@ -10,6 +10,7 @@ public class DeckSettingManager : MonoBehaviour
     [SerializeField] Canvas cardDictionaryCanvas;
     [SerializeField] Transform humanDeckList;
     [SerializeField] Transform orcDeckList;
+    [SerializeField] Transform pageButtons;
     [SerializeField] TMPro.TextMeshProUGUI humanDeckNum;
     [SerializeField] TMPro.TextMeshProUGUI orcDeckNum;
     [SerializeField] HUDController hudController;
@@ -17,6 +18,11 @@ public class DeckSettingManager : MonoBehaviour
     public MenuSceneController menuSceneController;
 
     MyDecksLoader decksLoader;
+
+    int humanDeckPage = 0;
+    int humanCurrentPage = 0;
+    int orcDeckPage = 0;
+    int orcCurrentPage = 0;
 
     public void AttachDecksLoader(ref MyDecksLoader decksLoader) {
         this.decksLoader = decksLoader;
@@ -51,31 +57,117 @@ public class DeckSettingManager : MonoBehaviour
     public void SetPlayerDecks() {
         int humanDeckCount = 0;
         int orcDeckCount = 0;
+        humanDeckPage = 0;
+        orcDeckPage = 0;
+        humanCurrentPage = 1;
+        orcCurrentPage = 1;
         int humanCustomDecks = AccountManager.Instance.humanDecks.Count;
         int orcCustomDecks = AccountManager.Instance.orcDecks.Count;
-        humanDeckList.GetChild(humanDeckCount).gameObject.SetActive(true);
-        orcDeckList.GetChild(orcDeckCount).gameObject.SetActive(true);
+        InitDecks();
+
         if (humanCustomDecks > 0) {
             for (int i = 0; i < humanCustomDecks; i++) {
-                humanDeckList.GetChild(humanDeckCount).gameObject.SetActive(true);
-                humanDeckList.GetChild(humanDeckCount).GetComponent<DeckHandler>().SetDeck(AccountManager.Instance.humanDecks[i]);
+                humanDeckList.GetChild(humanDeckPage).GetChild(humanDeckCount).gameObject.SetActive(true);
+                humanDeckList.GetChild(humanDeckPage).GetChild(humanDeckCount).GetComponent<DeckHandler>().SetDeck(AccountManager.Instance.humanDecks[i]);
                 humanDeckCount++;
-                if(humanDeckCount < 3)
-                    humanDeckList.GetChild(humanDeckCount).gameObject.SetActive(true);
+                if (humanDeckCount == 3) {
+                    humanDeckPage++;
+                    humanDeckCount = 0;
+                }
+                if (humanDeckPage < 3)
+                    humanDeckList.GetChild(humanDeckPage).GetChild(humanDeckCount).gameObject.SetActive(true);
             }
+            if (humanDeckPage == 3)
+                humanDeckPage--;
         }
         if (orcCustomDecks > 0) {
             for (int i = 0; i < orcCustomDecks; i++) {
-                orcDeckList.GetChild(orcDeckCount).gameObject.SetActive(true);
-                orcDeckList.GetChild(orcDeckCount).GetComponent<DeckHandler>().SetDeck(AccountManager.Instance.orcDecks[i]);
+                orcDeckList.GetChild(orcDeckPage).GetChild(orcDeckCount).gameObject.SetActive(true);
+                orcDeckList.GetChild(orcDeckPage).GetChild(orcDeckCount).GetComponent<DeckHandler>().SetDeck(AccountManager.Instance.orcDecks[i]);
                 orcDeckCount++;
-                if (orcDeckCount < 3)
-                    orcDeckList.GetChild(orcDeckCount).gameObject.SetActive(true);
+                if (orcDeckCount == 3) {
+                    orcDeckPage++;
+                    orcDeckCount = 0;
+                }
+                if (orcDeckPage < 3)
+                    orcDeckList.GetChild(orcDeckPage).GetChild(orcDeckCount).gameObject.SetActive(true);
+            }
+            if (orcDeckPage == 3)
+                orcDeckPage--;
+        }
+        humanDeckNum.text = "1/" + (humanDeckPage + 1).ToString();
+        orcDeckNum.text = "1/" + (orcDeckPage + 1).ToString();
+        pageButtons.Find("Human/Left").gameObject.SetActive(false);
+        if (humanDeckPage > 0)
+            pageButtons.Find("Human/Right").gameObject.SetActive(true);
+        else
+            pageButtons.Find("Human/Right").gameObject.SetActive(false);
+        pageButtons.Find("Orc/Left").gameObject.SetActive(false);
+        if (orcDeckPage > 0)
+            pageButtons.Find("Orc/Right").gameObject.SetActive(true);
+        else
+            pageButtons.Find("Orc/Right").gameObject.SetActive(false);
+    }
+
+    private void InitDecks() {
+        for (int i = 0; i < 3; i++) {
+            humanDeckList.GetChild(i).gameObject.SetActive(false);
+            orcDeckList.GetChild(i).gameObject.SetActive(false);
+            for(int j = 0; j < 3; j++) {
+                humanDeckList.GetChild(i).GetChild(j).gameObject.SetActive(false);
+                humanDeckList.GetChild(i).GetChild(j).Find("DeckInfo").gameObject.SetActive(false);
+                humanDeckList.GetChild(i).GetChild(j).Find("DeckInfo/EditButtons").gameObject.SetActive(false);
+                orcDeckList.GetChild(i).GetChild(j).gameObject.SetActive(false);
+                orcDeckList.GetChild(i).GetChild(j).Find("DeckInfo").gameObject.SetActive(false);
+                orcDeckList.GetChild(i).GetChild(j).Find("DeckInfo/EditButtons").gameObject.SetActive(false);
             }
         }
-        humanDeckNum.text = humanDeckCount.ToString() + "/3";
-        orcDeckNum.text = orcDeckCount.ToString() + "/3";
+        humanDeckList.GetChild(0).gameObject.SetActive(true);
+        orcDeckList.GetChild(0).gameObject.SetActive(true);
+        humanDeckList.GetChild(0).GetChild(0).gameObject.SetActive(true);
+        orcDeckList.GetChild(0).GetChild(0).gameObject.SetActive(true);
+    }
 
+    public void NextPageBtn(bool isHuman) {
+        if (isHuman) {
+            humanDeckList.GetChild(humanCurrentPage - 1).gameObject.SetActive(false);
+            humanDeckList.GetChild(humanCurrentPage).gameObject.SetActive(true);
+            humanCurrentPage++;
+            if (humanCurrentPage == 3 || !humanDeckList.GetChild(humanCurrentPage).GetChild(0).gameObject.activeSelf)
+                pageButtons.Find("Human/Right").gameObject.SetActive(false);
+            pageButtons.Find("Human/Left").gameObject.SetActive(true);
+            humanDeckNum.text = humanCurrentPage.ToString() + "/" + (humanDeckPage + 1).ToString();
+        }
+        else {
+            orcDeckList.GetChild(orcCurrentPage - 1).gameObject.SetActive(false);
+            orcDeckList.GetChild(orcCurrentPage).gameObject.SetActive(false);
+            orcCurrentPage++;
+            if (orcCurrentPage == 3 || !orcDeckList.GetChild(orcCurrentPage).GetChild(0).gameObject.activeSelf)
+                pageButtons.Find("Orc/Right").gameObject.SetActive(false);
+            pageButtons.Find("Orc/Left").gameObject.SetActive(true);
+            orcDeckNum.text = orcCurrentPage.ToString() + "/" + (orcDeckPage + 1).ToString();
+        }
+    }
+
+    public void PrevPageBtn(bool isHuman) {
+        if (isHuman) {
+            humanDeckList.GetChild(humanCurrentPage - 1).gameObject.SetActive(false);
+            humanDeckList.GetChild(humanCurrentPage - 2).gameObject.SetActive(true);
+            humanCurrentPage--;
+            if (humanCurrentPage == 1 || !humanDeckList.GetChild(humanCurrentPage - 1).GetChild(0).gameObject.activeSelf)
+                pageButtons.Find("Human/Left").gameObject.SetActive(false);
+            pageButtons.Find("Human/Right").gameObject.SetActive(true);
+            humanDeckNum.text = humanCurrentPage.ToString() + "/" + (humanDeckPage + 1).ToString();
+        }
+        else {
+            orcDeckList.GetChild(orcCurrentPage - 1).gameObject.SetActive(false);
+            orcDeckList.GetChild(orcCurrentPage - 2).gameObject.SetActive(true);
+            orcCurrentPage--;
+            if (orcCurrentPage == 1 || !orcDeckList.GetChild(orcCurrentPage-1).GetChild(0).gameObject.activeSelf)
+                pageButtons.Find("Orc/Left").gameObject.SetActive(false);
+            pageButtons.Find("Orc/Right").gameObject.SetActive(true);
+            orcDeckNum.text = orcCurrentPage.ToString() + "/" + (orcDeckPage + 1).ToString();
+        }
     }
 
     void OnDecksInfoLoaded() {
