290e6ecd80ab98ff46673c833a69d5a0fa2d0d75
Kwak Yong Ju <amese1225@fbl.kr>
2019-07-31 18:40:29 +0900

    skillAction 삭제

--- Assets/Script/Socket/BattleConnector_receiver.cs
+++ Assets/Script/Socket/BattleConnector_receiver.cs
@@ -230,7 +230,6 @@ public partial class BattleConnector : MonoBehaviour {
     }
 
     public IEnumerator waitSkillDone(UnityAction callback, bool isShield = false) {
-        PlayMangement.instance.skillAction = true;
         if(isShield) yield return new WaitForSeconds(2.0f);
         MagicDragHandler[] list = Resources.FindObjectsOfTypeAll<MagicDragHandler>();
         foreach(MagicDragHandler magic in list) {
@@ -250,7 +249,6 @@ public partial class BattleConnector : MonoBehaviour {
                 yield return new WaitForSeconds(1.0f);
             }
         }
-        PlayMangement.instance.skillAction = false;
         callback();
     }
 
