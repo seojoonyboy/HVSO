8827b23a25fc0e012eb3f99b7abdc2022b26ffcb
KHG <cow3323@fbl.kr>
2019-08-28 12:16:23 +0900

    재화 저장 클래스 생성, 창고 재고 충전 시간 관련 함수 구현

---
+++ Assets/Script/UserResourceManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using Sirenix.OdinInspector;
+[Sirenix.OdinInspector.ShowOdinSerializedPropertiesInInspector]
+
+public class UserResourceManager : SerializedMonoBehaviour {
+    // Start is called before the first frame update
+
+    public int gold;
+    public int crystal;
+    public int supplyStore;
+    public float timerTime;
+    public int supplyStoreTime;
+    public string supplyStoreTimer;
+    public int supply;
+    public int supplyBox;
+    
+
+    public void SetResource(int gold, int crystal, int supplyStore, int supplyStoreTime, int supply, int supplyBox) {
+        this.gold = gold;
+        this.crystal = crystal;
+        this.supplyStore = supplyStore;
+        SetTimer(supplyStoreTime);
+        this.supply = supply;
+        this.supplyBox = supplyBox;
+    }
+
+    private void Update() {
+        timerTime -= Time.deltaTime;
+        if((supplyStoreTime * 0.001f) - timerTime <= 1) {
+            SetTimer(supplyStoreTime - 1);
+        }
+    }
+
+    public void SetTimer(int supplyStoreTime) {
+        this.supplyStoreTime = supplyStoreTime;
+        timerTime = supplyStoreTime * 0.001f;
+        int leftSecond = (int)(supplyStoreTime * 0.001);
+        int leftMinute = (int)(leftSecond * 0.0167);
+        int leftHour = (int)(leftMinute * 0.0167);
+        supplyStoreTimer = leftHour.ToString() + ":" + leftMinute.ToString() + ":" + leftSecond.ToString();
+    }
+}
