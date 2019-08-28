29c3e0d8f64737374382191ac434d5e49d09dff1
Kwak Yong Ju <amese1225@fbl.kr>
2019-08-28 17:46:22 +0900

    시나리오 종족 선택버튼 활성화

--- Assets/Script/Scenario/ScenarioManager.cs
+++ Assets/Script/Scenario/ScenarioManager.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScenarioManager : MonoBehaviour
 {
     public static ScenarioManager Instance { get; private set; }
+    public ShowSelectRace human, orc;
 
     private void Awake() {
         Instance = this;
@@ -13,24 +15,42 @@ public class ScenarioManager : MonoBehaviour
     private void OnDestroy() {
         Instance = null;
     }   
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 
     public void OnBackButton() {
         SoundManager.Instance.PlaySound(SoundType.FIRST_TURN);
         FBL_SceneManager.Instance.LoadScene(FBL_SceneManager.Scene.MAIN_SCENE);
     }
 
+    public void OnHumanButton() {
+        orc.raceButton.GetComponent<Image>().sprite = orc.deactiveSprite;
+        orc.heroSelect.SetActive(false);
+        orc.StageCanvas.SetActive(false);
+
+        human.raceButton.GetComponent<Image>().sprite = human.activeSprite;
+        human.heroSelect.SetActive(true);
+        human.StageCanvas.SetActive(true);
+    }
+
+    public void OnOrcButton() {
+        human.raceButton.GetComponent<Image>().sprite = human.deactiveSprite;
+        human.heroSelect.SetActive(false);
+        human.StageCanvas.SetActive(false);
+
+        orc.raceButton.GetComponent<Image>().sprite = orc.activeSprite;
+        orc.heroSelect.SetActive(true);
+        orc.StageCanvas.SetActive(true);
+    }
 
 
+
+}
+
+[System.Serializable]
+public class ShowSelectRace {
+    public GameObject raceButton;
+    public Sprite activeSprite;
+    public Sprite deactiveSprite;
+    public GameObject heroSelect;
+    public GameObject StageCanvas;
 }
