87a8a03fb6d9819867e91c53b059af75eb592895
Kwak Yong Ju <amese1225@fbl.kr>
2019-09-04 14:27:35 +0900

    테스트 마스크 예시

--- Assets/Script/TestRaycast.cs
+++ Assets/Script/TestRaycast.cs
@@ -3,34 +3,94 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class TestRaycast : MonoBehaviour , IPointerClickHandler 
+public class TestRaycast : MonoBehaviour , IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public GameObject targetObject;    
+    Vector3 objectPosition;
+
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D[] hits = Physics2D.RaycastAll(
+        new Vector2(mousePos.x, mousePos.y),
+                Vector2.zero,
+                Mathf.Infinity
+            );
+
+
+
+        if (results.Exists(x => x.gameObject.name == "TargetScope")) {
+            if (results.Exists(x => x.gameObject.name == "TargetMask")) {
+
+                if (hits.Length > 0) {
+                    targetObject = hits[0].transform.gameObject;
+                    objectPosition = targetObject.transform.position;
+                }
+                        
+            }
+        }   
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {        
+    public void OnDrag(PointerEventData eventData) {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (targetObject != null)
+            targetObject.transform.position = mousePos;
+
     }
 
-    public void OnPointerClick(PointerEventData eventData) {
+    public void OnEndDrag(PointerEventData eventData) {
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
 
-        List<RaycastResult> result = new List<RaycastResult>();
-        EventSystem.current.RaycastAll(eventData, result);
+        if (results.Exists(x => x.gameObject.name == "TargetScope")) {
+            if (results.Exists(x => x.gameObject.name == "TargetMask")) {
 
-        if (result.Exists(x => x.gameObject.name == "TargetScope")) {
-            if (result.Exists(x => x.gameObject.name == "TargetMask")) {
-                Debug.Log("통과!");
+                if (targetObject != null) {
+                    Vector2 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    targetObject.transform.position = position;
+                }
             }
             else
-                Debug.Log("하나!");            
+                targetObject.transform.position = objectPosition;
         }
-
-
+        else
+            targetObject.transform.position = objectPosition;
+        
+        targetObject = null;
     }
+
+
+
+
+    //public void OnPointerClick(PointerEventData eventData) {
+
+    //    List<RaycastResult> result = new List<RaycastResult>();
+    //    EventSystem.current.RaycastAll(eventData, result);
+
+    //    Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);       
+    //    RaycastHit2D[] hits = Physics2D.RaycastAll(
+    //        new Vector2(mousePos.x, mousePos.y),
+    //        Vector2.zero,
+    //        Mathf.Infinity
+    //    );
+
+
+
+    //    if (result.Exists(x => x.gameObject.name == "TargetScope")) {
+    //        if (result.Exists(x => x.gameObject.name == "TargetMask")) {
+
+    //            if (hits.Length > 0)
+    //                Debug.Log(hits[0].transform.gameObject.name);
+    //        }
+    //    }
+
+
+    //}
 
 
 
