4fefc3513fb7a38a7196e653aad8983b1bef0878
joonyboy <seojoonwon@naver.com>
2019-09-08 21:23:49 +0900

    HorizontalScrollSnap이 Content의 Child의 RectTransform을 초기화 시켜준 이후에 SafeArea 영역을 찾아 그만큼 SafeAreaScrollSnap에서 ScrollSnap의 Content Child의 RectTransform을 재조정하도록 함

--- Assets/Plugins/unity-ui-extensions/Scripts/Layout/HorizontalScrollSnap.cs
+++ Assets/Plugins/unity-ui-extensions/Scripts/Layout/HorizontalScrollSnap.cs
@@ -2,15 +2,16 @@
 /// Sourced from - http://forum.unity3d.com/threads/scripts-useful-4-6-scripts-collection.264161/page-2#post-1945602
 /// Updated by ddreaper - removed dependency on a custom ScrollRect script. Now implements drag interfaces and standard Scroll Rect.
 
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 namespace UnityEngine.UI.Extensions
 {
-
     [RequireComponent(typeof(ScrollRect))]
     [AddComponentMenu("Layout/Extensions/Horizontal Scroll Snap")]
     public class HorizontalScrollSnap : ScrollSnapBase, IEndDragHandler
     {
+        public UnityEvent OnUpdateLayoutCalled = new UnityEvent();
         void Start()
         {
             _isVertical = false;
@@ -211,6 +212,8 @@ namespace UnityEngine.UI.Extensions
                 UpdateVisible();
             SetScrollContainerPosition();
             OnCurrentScreenChange(_currentPage);
+
+            OnUpdateLayoutCalled.Invoke();
         }
 
         private void OnRectTransformDimensionsChange()
