1fdb8e1d66d87bab19fd6ce1795dd637f5a75f51
Kwak Yong Ju <amese1225@fbl.kr>
2019-09-16 19:21:11 +0900

    실드게이지 꽉 차는 부분 코르틴 추가

--- Assets/Script/Scenario/ScenarioExecute.cs
+++ Assets/Script/Scenario/ScenarioExecute.cs
@@ -246,6 +246,11 @@ public class Fill_shield_gage : ScenarioExecute {
     public Fill_shield_gage() : base() { }
 
     public override void Execute() {
+        StartCoroutine(Fill_gage());
+    }
+
+    IEnumerator Fill_gage() {
+        yield return new WaitUntil(() => PlayMangement.instance.player.shieldStack.Value >= 1);
         PlayMangement.instance.player.FullShieldStack(PlayMangement.instance.player.shieldStack.Value);
         PlayMangement.instance.player.shieldStack.Value = 8;
         handler.isDone = true;
