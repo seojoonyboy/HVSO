5fad741379a3b4b3a2353f90e6a3d40a649a25ab
joonyboy <seojoonwon@naver.com>
2019-09-19 18:03:42 +0900

    인게임 설정 모달과 escape 버튼 연동 완료. 시안 이미지 제거. 닫기 버튼 이미지 리소스 추가.

--- Assets/Script/Ingame/IngameSettingModalManager.cs
+++ Assets/Script/Ingame/IngameSettingModalManager.cs
@@ -3,19 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class IngameSettingModalManager : MonoBehaviour {
+    [SerializeField] GameObject basePanel;
+    [SerializeField] GameObject settingModal, quitModal;
+
     void Update() {
         if (Input.GetKeyDown(KeyCode.Escape)) {
-            if (!gameObject.activeSelf) {
-                gameObject.SetActive(true);
+            if(!basePanel.activeSelf) basePanel.SetActive(true);
+
+            if (!settingModal.activeSelf) {
+                settingModal.SetActive(true);
             }
             else {
-                SurrendModal();
+                quitModal.SetActive(true);
             }
-            
         }
     }
 
-    private void SurrendModal() {
+    public void OnSurrendBtn() {
+        quitModal.SetActive(true);
+    }
 
+    public void OnCancelBtn() {
+        OffAllModals();
+    }
+
+    private void OffAllModals() {
+        quitModal.SetActive(false);
+        settingModal.SetActive(false);
+        basePanel.SetActive(false);
     }
 }
