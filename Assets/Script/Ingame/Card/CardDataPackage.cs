51975053995b516d278940a9bcf501daf1020841
KHG <cow3323@fbl.kr>
2019-09-19 17:21:14 +0900

    도감 버튼 총 카드 갯수 표기 수정, 새로운 카드 확인 표기 시스템 변경

--- Assets/Script/Ingame/Card/CardDataPackage.cs
+++ Assets/Script/Ingame/Card/CardDataPackage.cs
@@ -8,7 +8,9 @@ using Sirenix.OdinInspector;
 [CreateAssetMenu]
 [Sirenix.OdinInspector.ShowOdinSerializedPropertiesInInspector]
 public class CardDataPackage : ScriptableObject{
-    public string ClientVersion;
+    public string deviceId;
     public Dictionary<string, CardData> data = new Dictionary<string, CardData>();
+    public List<string> checkHumanCard;
+    public List<string> checkOrcCard;
 }
 
