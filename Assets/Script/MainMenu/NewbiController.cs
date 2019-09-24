c087a3abf837fe6559d861439ec06712fffde624
이종욱 <wjlee729@fbl.kr>
2019-09-24 12:05:16 +0900

    NewbiController lamda delgate remove 처리

--- Assets/Script/MainMenu/NewbiController.cs
+++ Assets/Script/MainMenu/NewbiController.cs
@@ -40,9 +40,12 @@ public class NewbiController : MonoBehaviour {
 
         if (unityEvent == null) return;
         preProcess.Add(eventName);
-        unityEvent.AddListener(() => {
+        UnityAction action = null;
+        action = () => {
             OnEventOccured(eventName);
-        });
+            unityEvent.RemoveListener(action);
+        };
+        unityEvent.AddListener(action);
     }
 
     private void OnEventOccured(string eventName) {
@@ -104,10 +107,5 @@ public class NewbiController : MonoBehaviour {
 
         //Wait Until Ingame Muligun Begin
         AddProcess("WaitSec");
-    }
-
-    void OnDestroy() {
-        ScenarioManager.OnLobbySceneLoaded.RemoveAllListeners();
-        BattleConnector.OnOpenSocket.RemoveAllListeners();
     }
 }
