8bc592bf706f0b2987d09b86eddbb56838923f0d
joonyboy <seojoonwon@naver.com>
2019-09-24 15:50:57 +0900

    유닛에게 체인 속성을 부여할 때 유닛에 속성 자체가 없는 경우 발생하는 에러 수정.

--- Assets/Script/Ingame/AboutSkill/SkillComp/0611Renewal/Attributes/chain.cs
+++ Assets/Script/Ingame/AboutSkill/SkillComp/0611Renewal/Attributes/chain.cs
@@ -7,6 +7,9 @@ namespace SkillModules {
         // Start is called before the first frame update
         void Start() {
             var placeMonsterComp = GetComponent<PlaceMonster>();
+            if(placeMonsterComp.unit.attributes == null) {
+                placeMonsterComp.unit.attributes = new string[1];
+            }
             int index = placeMonsterComp.unit.attributes.Length;
             placeMonsterComp.unit.attributes[index] = "chain";
         }
