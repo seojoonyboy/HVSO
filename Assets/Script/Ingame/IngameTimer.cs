d088ed53fe9374ce56cb02fc1fa3440e08b443b8
joonyboy <seojoonwon@naver.com>
2019-09-26 17:36:44 +0900

    인게임 타이머 수정. 내부에서 전체 대기시간 및 UI등장 처리까지 포함하여 처리하게 수정

--- Assets/Script/Ingame/IngameTimer.cs
+++ Assets/Script/Ingame/IngameTimer.cs
@@ -6,37 +6,69 @@ using TMPro;
 public class IngameTimer : MonoBehaviour {
     [SerializeField] GameObject timerUI;
     [SerializeField] TextMeshProUGUI value;
-    public int startSec = 10;
+    public int uiStartSec = 10;
+
     int currentSec;
-    IEnumerator coroutine;
-    public void OnTimerUI() {
+    IEnumerator TimerCoroutine, UICoroutine;
+
+    private void OnTimerUI() {
+        Logger.Log("Timer UI 시작");
         timerUI.SetActive(true);
 
-        coroutine = TimerOn();
-        StartCoroutine(coroutine);
+        UICoroutine = TimerUIOn();
+        StartCoroutine(UICoroutine);
     }
 
-    IEnumerator TimerOn() {
-        currentSec = startSec;
+    /// <summary>
+    /// UI 타이머 Coroutine
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator TimerUIOn() {
         while (currentSec > 0) {
             value.text = currentSec.ToString();
             yield return new WaitForSeconds(1.0f);
             currentSec -= 1;
         }
-        OffTimerUI();
+        EndTimer();
     }
 
-    public void OffTimerUI() {
-        timerUI.SetActive(false);
+    /// <summary>
+    /// 전체 타이머 Coroutine
+    /// </summary>
+    /// <param name="totalSec"></param>
+    /// <returns></returns>
+    IEnumerator TimerOn(int totalSec) {
+        currentSec = totalSec;
+        while (currentSec > uiStartSec) {
+            Logger.Log("Timer : " + currentSec);
+            yield return new WaitForSeconds(1.0f);
+            currentSec -= 1;
+        }
 
-        if (coroutine == null) return;
-        StopCoroutine(coroutine);
+        OnTimerUI();
+    }
+
+    /// <summary>
+    /// 타이머 시작
+    /// </summary>
+    /// <param name="totalSec"></param>
+    public void BeginTimer(int totalSec = 40) {
+        TimerCoroutine = TimerOn(totalSec);
+        StartCoroutine(TimerCoroutine);
+    }
+
+    /// <summary>
+    /// 타이머 종료
+    /// </summary>
+    public void EndTimer() {
+        timerUI.SetActive(false);
+        StopAllCoroutines();
     }
 
 #if UNITY_EDITOR
     void Update() {
         if (Input.GetKeyDown(KeyCode.T)) {
-            OnTimerUI();
+            BeginTimer();
         }
     }
 #endif
