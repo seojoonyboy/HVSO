c1bab63d2ee80a89750c6af62cc50100020414be
joonyboy <seojoonwon@naver.com>
2019-09-27 15:11:22 +0900

    로그인 종류 선택 모달 폰트 깨짐 수정. 공용 모달 레이아웃 수정.

--- Assets/Script/ETC/Modal.cs
+++ Assets/Script/ETC/Modal.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 using UnityEngine.Events;
 
 public class Modal : MonoBehaviour {
+    [SerializeField] private GameObject InnerModal;
 	[SerializeField] private Button yesButton;
 	[SerializeField] private Button noButton;
 	[SerializeField] private Button closeBtn;
@@ -12,6 +13,9 @@ public class Modal : MonoBehaviour {
 	[SerializeField] private GameObject inputGameObject;
 	[SerializeField] private Text insertTitle;
 	[SerializeField] private InputField inputField;
+    const float DefaultHeight = 455f;
+    const float DefaultWidth = 745f;
+    const float InputHeight = 550f;
 
 	public enum Type {
 		YESNO,
@@ -26,7 +30,7 @@ public class Modal : MonoBehaviour {
 	/// <param name="function">yes 버튼 누를 경우 실행 함수(필요하면)</param>
 	/// <param name="title">제목에 들어갈 내용(필요하면)(급하게 넣은 매개변수)</param>
 	public static void instantiate(string text, Type type, UnityAction function = null, string title = null) {
-		if(type == Type.INSERT) {
+        if (type == Type.INSERT) {
 			Logger.LogWarning("enum INSERT는 매개변수 하나 더 있습니다!");
 			return;
 		}
@@ -36,6 +40,7 @@ public class Modal : MonoBehaviour {
             Logger.LogError("no Canvas");
             return;
         }
+        modal.GetComponent<Modal>().InnerModal.GetComponent<RectTransform>().sizeDelta = new Vector2(DefaultWidth, DefaultHeight);
         Instantiate(modal).GetComponent<Modal>().SetData(text, function, type, title);
         //Instantiate(modal, canvas.transform, false).GetComponent<Modal>().setData(text, function, type);
 	}
@@ -60,31 +65,7 @@ public class Modal : MonoBehaviour {
         }
         GameObject tmp = Instantiate(modal);
         tmp.GetComponent<Modal>().SetData(text, descText, inputText, function);
-        return tmp;
-        //Instantiate(modal, canvas.transform, false).GetComponent<Modal>().setData(text, inputText, function);
-	}
-
-    /// <summary>
-    /// Modal 창 생성기 (Insert 편) + 창닫기 버튼 추가
-    /// </summary>
-    /// <param name="text">제목 들어갈 내용</param>
-    /// <param name="descText">inputField 빈공간에 들어갈 내용</param>
-    /// <param name="inputText">inputField value</param>
-    /// <param name="type">Modal.Type.종류</param>
-    /// <param name="function">yes 버튼 누를 경우 실행 함수</param>
-    public static GameObject instantiateWithClose(string text, string descText, string inputText, Type type, UnityAction<string> function) {
-		if(type != Type.INSERT) {
-			Logger.LogWarning("enum YESNO 또는 CHECK는 매개변수를 줄여주십시오!");
-			return null;
-		}
-		GameObject modal = Resources.Load("Prefabs/ModalCanvas", typeof(GameObject)) as GameObject;
-        Canvas canvas = (Canvas)FindObjectOfType(typeof(Canvas));
-        if(canvas == null) {
-            Logger.LogError("no Canvas");
-            return null;
-        }
-        GameObject tmp = Instantiate(modal);
-        tmp.GetComponent<Modal>().SetData(text, descText, inputText, function, true);
+        tmp.GetComponent<Modal>().InnerModal.GetComponent<RectTransform>().sizeDelta = new Vector2(DefaultWidth, InputHeight);
         return tmp;
         //Instantiate(modal, canvas.transform, false).GetComponent<Modal>().setData(text, inputText, function);
 	}
@@ -94,6 +75,13 @@ public class Modal : MonoBehaviour {
         return modal;
     }
 
+    /// <summary>
+    /// Modal 창 생성기 (YESNO와 CHECK 편)에 대한 데이터 세팅
+    /// </summary>
+    /// <param name="text">설명에 들어갈 내용</param>
+	/// <param name="type">Modal.Type.종류</param>
+	/// <param name="function">yes 버튼 누를 경우 실행 함수(필요하면)</param>
+	/// <param name="title">제목에 들어갈 내용(필요하면)(급하게 넣은 매개변수)</param>
 	public void SetData(string text, UnityAction function, Type type, string title = null) {
 		if(type == Type.CHECK) {
 			noButton.gameObject.SetActive(false);
