e7b03442651e2c2f2a36e1a82083703e1c08b4cb
Kwak Yong Ju <amese1225@fbl.kr>
2019-10-02 19:02:25 +0900

    튜토리얼신 누락 수정, 스크립트 마지막에 end_tutorial 추가, 유닛추가 수정

--- Assets/Script/Scenario/ScenarioExecute.cs
+++ Assets/Script/Scenario/ScenarioExecute.cs
@@ -651,9 +651,10 @@ public class Reinforement_Unit : ScenarioExecute {
 
         for (int i = 0; i < 5; i++) {
             
-            scenarioGameManagment.SummonUnit(scenarioGameManagment.player.isPlayer, back, 0, i);
-            scenarioGameManagment.SummonUnit(scenarioGameManagment.player.isPlayer, front, 1, i);
+            scenarioGameManagment.SummonUnit(scenarioGameManagment.player.isPlayer, back, i, 0);
+            scenarioGameManagment.SummonUnit(scenarioGameManagment.player.isPlayer, front, i, 1);
         }
+        handler.isDone = true;
     }
 
 
