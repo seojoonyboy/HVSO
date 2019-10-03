0e7af6a4ad53e598edb110893a221fd7073677c3
joonyboy <seojoonwon@naver.com>
2019-10-03 22:47:46 +0900

    로그인 화면에서 텍스트 등장하는 시간 앞당김

--- Assets/Script/MainMenu/Controllers/LoginController.cs
+++ Assets/Script/MainMenu/Controllers/LoginController.cs
@@ -17,7 +17,7 @@ public class LoginController : MonoBehaviour {
     }
 
     IEnumerator LogoReveal() {
-        yield return new WaitForSeconds(4.0f);
+        yield return new WaitForSeconds(3.0f);
         logo.SetActive(true);
         Logger.Log("logo");
         yield return new WaitForSeconds(0.5f);
