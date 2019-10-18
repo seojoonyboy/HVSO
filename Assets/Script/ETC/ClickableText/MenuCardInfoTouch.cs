35810cbd31774be50186f4c46b4a64ba2a937153
이종욱 <wjlee729@fbl.kr>
2019-10-18 18:50:17 +0900

    유닛 상세보기 스킬 클릭 시 설명 모달 띄우기

---
+++ Assets/Script/ETC/ClickableText/MenuCardInfoTouch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+public partial class MenuCardInfo : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+
+    [SerializeField] private TextMeshProUGUI dialogText;
+    private bool modalOn = false;
+
+    public void OnPointerDown(PointerEventData eventData) {
+        if(modalOn) return;
+        modalOn = true;
+
+        int linkIndex = TMP_TextUtilities.FindIntersectingLink(dialogText, Input.mousePosition, null);
+
+        if (linkIndex > -1) {
+            var linkInfo = dialogText.textInfo.linkInfo[linkIndex];
+            var linkId = linkInfo.GetLinkID();
+            var data = translator.GetTranslatedSkillTypeDesc(linkId);
+            OpenClassDescModal(linkId, accountManager.resource.skillIcons[linkId]);
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData) {
+        CloseClassDescModal();
+        modalOn = false;
+    }
+}
