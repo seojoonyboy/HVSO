86fdc18a4245744287bcd5ed25c759eb155f330d
joonyboy <seojoonwon@naver.com>
2019-11-08 15:41:51 +0900

    로고가 너무 늦게 뜨는 문제 수정. (해긴 스플래시가 OBB씬에서 재생되어 문제 발생. 예전에는 Login씬에서 재생되었음)

--- Assets/Script/MainMenu/Controllers/LoginController.cs
+++ Assets/Script/MainMenu/Controllers/LoginController.cs
@@ -53,7 +53,7 @@ public class LoginController : MonoBehaviour {
     }
 
     IEnumerator LogoReveal() {
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(0.8f);
         logo.SetActive(true);
         Logger.Log("logo");
         yield return new WaitForSeconds(0.5f);
