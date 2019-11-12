42d9a63f53da2c5de16cdef8463636f702a6dddb
Kwak Yong Ju <amese1225@fbl.kr>
2019-11-12 14:23:04 +0900

    스킬 아이콘을 길게 누르라는 문구 추가

--- Assets/Script/Scenario/ScenarioExecute.cs
+++ Assets/Script/Scenario/ScenarioExecute.cs
@@ -1434,6 +1434,7 @@ public class Focus_Skill_Icon : ScenarioExecute {
         parseTime = int.Parse(args[0]);
         time = parseTime - 0.3f;
         PlayMangement.instance.EventHandler.AddListener(IngameEventHandler.EVENT_TYPE.CLICK_SKILL_ICON, CheckClick);
+        scenarioMask.outText.gameObject.SetActive(true);
 
         click = Observable.EveryUpdate().Where(_ => clickIcon == true).Select(_ => currentTime += Time.deltaTime).SkipWhile(x => x < time).First().Subscribe(_ => ClickFinish());
         unclick = Observable.EveryUpdate().Where(_ => clickIcon == true).Where(_ => Input.GetMouseButton(0) == false).Subscribe(_ => { currentTime = 0; clickIcon = false; });
@@ -1446,6 +1447,7 @@ public class Focus_Skill_Icon : ScenarioExecute {
     private void ClickFinish() {
         click.Dispose();
         unclick.Dispose();
+        scenarioMask.outText.gameObject.SetActive(false);
         PlayMangement.instance.EventHandler.AddListener(IngameEventHandler.EVENT_TYPE.CLICK_SKILL_ICON, CheckClick);
         handler.isDone = true;
     }
