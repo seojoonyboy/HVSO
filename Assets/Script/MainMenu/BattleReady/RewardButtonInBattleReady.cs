f2ff3e4cdf4bb41f0986beef1b3200628f9b6036
joonyboy <seojoonwon@naver.com>
2019-11-22 17:49:05 +0900

    누락 스크립트 추가

---
+++ Assets/Script/MainMenu/BattleReady/RewardButtonInBattleReady.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using System;
+
+public class RewardButtonInBattleReady : MonoBehaviour {
+    Image image;
+    IDisposable observer;
+    [SerializeField] RewardProgressController progressController;
+    [SerializeField] GameObject progressBar;
+    [SerializeField] Sprite[] toggleImages;
+
+    RectTransform rect;
+    private void Awake() {
+        rect = GetComponent<RectTransform>();
+        image = transform.Find("Image").GetComponent<Image>();
+    }
+
+    IEnumerator Start() {
+        yield return new WaitForEndOfFrame();
+        MMRChanged();
+
+        observer = Observable
+            .EveryUpdate()
+            .Where(x => progressController.isMoving == true)
+            .Subscribe(_ => MMRChanged());
+    }
+
+    private void MMRChanged() {
+        var barX = progressBar.GetComponent<RectTransform>().rect.width;
+        Logger.Log("barX : " + barX);
+
+        if(rect.localPosition.x >= barX) {
+            image.sprite = toggleImages[0];
+        }
+        else {
+            image.sprite = toggleImages[1];
+        }
+    }
+
+    private void OnDestroy() {
+        if(observer != null) observer.Dispose();
+    }
+}
