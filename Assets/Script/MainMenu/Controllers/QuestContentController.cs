024e6da82d7620d9377c18051e82120f137985a8
이종욱 <wjlee729@fbl.kr>
2019-12-08 15:23:50 +0900

    Quest 관련 스크립트 초안 추가

---
+++ Assets/Script/MainMenu/Controllers/QuestContentController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using TMPro;
+using System.Reflection;
+
+public class QuestContentController : MonoBehaviour {
+    
+    [SerializeField] private TextMeshProUGUI title;
+    [SerializeField] private TextMeshProUGUI info;
+    [SerializeField] private Slider slider;
+    [SerializeField] private TextMeshProUGUI sliderInfo;
+    [SerializeField] private Button getBtn;
+    [SerializeField] private Button rerollBtn;
+
+    public QuestData data;
+    
+    private void Start() {
+        if(data == null) return;
+        title.text = data.title;
+        info.text = data.info;
+        slider.maxValue = (float)data.reachNum;
+        slider.value = (float)data.currentNum;
+        if(data.isClear) {
+            getBtn.enabled = false;
+            getBtn.GetComponentInChildren<TextMeshProUGUI>().text = "진행중";
+        }
+        else {
+            getBtn.enabled = false;
+            getBtn.GetComponentInChildren<TextMeshProUGUI>().text = "획득하기";
+        }
+    }
+
+    private void GetReward() {
+        gameObject.SetActive(false);
+    }
+
+    public void ActiveTutorial() {
+        for(int i = 0; i < data.tutorials.Length; i++) {
+            MethodInfo theMethod = this.GetType().GetMethod(data.tutorials[i].method);
+            object[] args = data.tutorials[i].args;
+            theMethod.Invoke(this, args);
+        }
+    }
+}
---
+++ Assets/Script/MainMenu/Managers/QuestManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using dataModules;
+using System;
+
+public class QuestManager : MonoBehaviour
+{
+    [SerializeField] Transform content;
+    private List<QuestContentController> quests;
+
+
+    private void Awake() {
+        quests = new List<QuestContentController>();
+        content.GetComponentsInChildren<QuestContentController>(true, quests);
+        Debug.Log(quests.Count);
+    }
+
+    public void AddQuest(string data) {
+        QuestContentController quest = quests.Find(x=>!x.gameObject.activeSelf);
+        quest.data = JsonReader.Read<QuestData>(data);
+        quest.gameObject.SetActive(true);
+        if(quest.data.tutorials == null) return;
+        quest.ActiveTutorial();
+    }
+}
+
+[Serializable] public class QuestData {
+    public string title;
+    public string info;
+    public int reachNum;
+    public int currentNum;
+    public bool isClear = false;
+    public TutorialShowList[] tutorials;
+}
+
+public class TutorialShowList {
+    public string method;
+    public string[] args;
+}
