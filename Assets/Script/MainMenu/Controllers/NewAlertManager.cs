a9618503364ba1950fdd3cbfe377098a12564169
Kwak Yong Ju <amese1225@fbl.kr>
2019-12-13 16:53:37 +0900

    ! 표시 작업중 (false 아님)

--- Assets/Script/MainMenu/Controllers/NewAlertManager.cs
+++ Assets/Script/MainMenu/Controllers/NewAlertManager.cs
@@ -8,7 +8,9 @@ public class NewAlertManager : MonoBehaviour
 {
     public static NewAlertManager Instance;
     public GameObject alertPref;
-
+    public static List<string> buttonName;
+    public string iconName = "NewAlertIcon";
+    
     private void Awake() {
         Instance = this;
     }
@@ -19,10 +21,34 @@ public class NewAlertManager : MonoBehaviour
     }
 
     public void SetUpButtonToAlert(GameObject button) {
-        
+        Button targetButton = button.GetComponent<Button>();
+        if (targetButton == null) return;
+        if (button.transform.Find(iconName).gameObject != null) return;
 
+        RectTransform rect = button.GetComponent<RectTransform>();
+        Vector3[] buttonRect = new Vector3[4];
+        rect.GetWorldCorners(buttonRect);
+
+        Vector3 iconPos = new Vector3(rect.position.x + buttonRect[2].x / 2, buttonRect[2].y, 0);
+
+        GameObject Icon = Instantiate(alertPref, rect);
+        Icon.transform.position = iconPos;
+        Icon.name = iconName;
+
+        buttonName.Add(button.name);
+        targetButton.OnClickAsObservable().Subscribe(_ => DisableButtonToAlert(button)).AddTo(Icon);
+        Icon.SetActive(true);
     }
 
+    public void DisableButtonToAlert(GameObject button) {
+        if (button.GetComponent<Button>() == null) return;
+        Transform icon = button.transform.Find(iconName);
+        if (icon == null) return;
+        buttonName.Remove(button.name);
+        Destroy(icon.gameObject);
+    }
+}
 
-
+public class AlartNameData {
+    public List<string> menuNameList;
 }
