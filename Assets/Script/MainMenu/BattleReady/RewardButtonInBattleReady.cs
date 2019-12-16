29f3573abca3a54f9a53e60d2960441b0689a026
joonyboy <seojoonwon@naver.com>
2019-12-16 19:29:56 +0900

    대전준비 화면에 개별 보상 요청 구현 완료. 또한 역대 최고점 MMR UI 적으로 게이지보여줌.

--- Assets/Script/MainMenu/BattleReady/RewardButtonInBattleReady.cs
+++ Assets/Script/MainMenu/BattleReady/RewardButtonInBattleReady.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UniRx;
 using System;
+using BestHTTP;
 
 public class RewardButtonInBattleReady : MonoBehaviour {
     Image image;
@@ -13,12 +14,20 @@ public class RewardButtonInBattleReady : MonoBehaviour {
     [SerializeField] Sprite[] toggleImages;
 
     RectTransform rect;
+    public AccountManager.Reward rewardData;
+
+    bool rewardAvailable;
+    bool rewardDataLoaded;
+
     private void Awake() {
         rect = GetComponent<RectTransform>();
         image = transform.Find("Image").GetComponent<Image>();
+
+        rewardAvailable = false;
     }
 
     IEnumerator Start() {
+        yield return new WaitUntil(() => rewardDataLoaded);
         yield return new WaitForEndOfFrame();
         MMRChanged();
 
@@ -28,18 +37,62 @@ public class RewardButtonInBattleReady : MonoBehaviour {
             .Subscribe(_ => MMRChanged());
     }
 
+    private Color32 deactiveColor = new Color32(70, 70, 70, 255);
+    private Color32 activeColor = new Color32(255, 255, 255, 255);
     private void MMRChanged() {
         var barX = progressBar.GetComponent<RectTransform>().rect.width + 50;
 
-        if(rect.localPosition.x >= barX) {
+        //조건이 안됨
+        if (!rewardData.canClaim) {
             image.sprite = toggleImages[0];
+            image.color = activeColor;
+            transform.Find("CheckMark").gameObject.SetActive(false);
+            rewardAvailable = false;
         }
+        //조건이 됨
         else {
-            image.sprite = toggleImages[1];
+            //받지 않았음
+            if (!rewardData.claimed) {
+                image.sprite = toggleImages[1];
+                image.color = activeColor;
+                transform.Find("CheckMark").gameObject.SetActive(false);
+                rewardAvailable = true;
+            }
+            //이미 받았음
+            else {
+                image.sprite = toggleImages[0];
+                image.color = deactiveColor;
+                transform.Find("CheckMark").gameObject.SetActive(true);
+                rewardAvailable = false;
+            }
         }
     }
 
     private void OnDestroy() {
         if(observer != null) observer.Dispose();
+    }
+
+    public void RequestReward() {
+        if (!rewardAvailable) return;
+
+        int id = GetComponentInChildren<dataModules.IntergerIndex>().Id;
+        AccountManager.Instance.RequestLeagueReward(OnRewardCallback, id);
+    }
+
+    private void OnRewardCallback(HTTPRequest originalRequest, HTTPResponse response) {
+        Logger.Log("OnRewardCallback : " + response.DataAsText);
+        if(response.DataAsText.Contains("not allowed")) {
+            Modal.instantiate("요청 불가", Modal.Type.CHECK);
+        }
+        else {
+            Modal.instantiate("우편으로 발송되었습니다.", Modal.Type.CHECK, () => {
+
+            });
+        }
+    }
+
+    public void SetRewardData(AccountManager.Reward reward) {
+        rewardData = reward;
+        rewardDataLoaded = true;
     }
 }
