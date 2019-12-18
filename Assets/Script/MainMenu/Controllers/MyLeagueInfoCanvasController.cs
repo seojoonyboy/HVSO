6577b892d961357a58dc47db15be1674c970701e
joonyboy <seojoonwon@naver.com>
2019-12-18 11:35:30 +0900

    메인화면 및 대전준비화면에 내 리그 정보 화면으로 이동하는 버튼 추가함.

--- Assets/Script/MainMenu/Controllers/MyLeagueInfoCanvasController.cs
+++ Assets/Script/MainMenu/Controllers/MyLeagueInfoCanvasController.cs
@@ -7,9 +7,50 @@ public class MyLeagueInfoCanvasController : MonoBehaviour {
     [SerializeField] Transform rankingContent;
     [SerializeField] GameObject mmrSliderArea;
     [SerializeField] GameObject rankingPool;
-
+    [SerializeField] HUDController hudController;
+    [SerializeField] BattleReadySceneController battleReadySceneController;
     // Start is called before the first frame update
     void Start() {
 
+    }
+
+    void OnPanel() {
+        gameObject.SetActive(true);
+
+        EscapeKeyController.escapeKeyCtrl.AddEscape(OffPanel);
+    }
+
+    public void OnPanelByMain() {
+        hudController.SetHeader(HUDController.Type.RESOURCE_ONLY_WITH_BACKBUTTON);
+        hudController.SetBackButton(() => {
+            OffPanelByMain();
+        });
+
+        OnPanel();
+    }
+
+    public void OnPanelByBattleReady() {
+        hudController.SetHeader(HUDController.Type.RESOURCE_ONLY_WITH_BACKBUTTON);
+        hudController.SetBackButton(() => {
+            OffPanelByBattleReady();
+        });
+
+        OnPanel();
+    }
+
+    void OffPanel() {
+        gameObject.SetActive(false);
+
+        EscapeKeyController.escapeKeyCtrl.RemoveEscape(OffPanel);
+    }
+
+    public void OffPanelByBattleReady() {
+        hudController.SetBackButton(() => battleReadySceneController.OnBackButton());
+        OffPanel();
+    }
+
+    public void OffPanelByMain() {
+        hudController.SetHeader(HUDController.Type.SHOW_USER_INFO);
+        OffPanel();
     }
 }
