e743fe8eb17f7824005ae96748f78b2d6798bab7
KHG <cow3323@fbl.kr>
2019-12-19 16:50:07 +0900

    영웅 정보화면 특성 이미지 표기 임시 구현 완료

--- Assets/Script/ResourceManager.cs
+++ Assets/Script/ResourceManager.cs
@@ -27,6 +27,7 @@ public class ResourceManager : SerializedMonoBehaviour
     public Dictionary<string, Sprite> campBackgrounds;   //진영별 뒷배경
     public Dictionary<string, Sprite> rewardIcon;
     public Dictionary<string, Sprite> rankIcons;
+    public Dictionary<string, Sprite> traitIcons;
 
     public Dictionary<string, UnitRace> unitRace;
 
