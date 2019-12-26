b0f816b14eb82a2c5c129f4c3130bf490e83c5d0
이종욱 <wjlee729@fbl.kr>
2019-12-26 11:26:22 +0900

    안드로이드 permission 허가 세팅 후 obb 다운으로 세팅

--- Assets/Haegin/Sample/Scenes/SceneOBBCheckController.cs
+++ Assets/Haegin/Sample/Scenes/SceneOBBCheckController.cs
@@ -144,9 +144,9 @@ public class SceneOBBCheckController : MonoBehaviour {
 
         UGUICommon.ShowRequestPermission(requestPermissionDialog, canvas, TextManager.GetString(TextManager.StringTag.AppPermissionsTitle1), TextManager.GetString(TextManager.StringTag.AppPermissionsInfo1), (UGUICommon.ButtonType buttonType) =>
         {
-            //RequestAndroidPermission(0);
-            gameObject.SetActive(false);
-            FindObjectOfType<LoginController>().sceneLoginCanvas.gameObject.SetActive(true);
+            RequestAndroidPermission(0);
+            //gameObject.SetActive(false);
+            //FindObjectOfType<LoginController>().sceneLoginCanvas.gameObject.SetActive(true);
         });
     }
 
