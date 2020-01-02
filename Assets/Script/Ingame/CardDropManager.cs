067287f1ae406f6dcb226184900697b115bb01b2
Kwak Yong Ju <amese1225@fbl.kr>
2020-01-02 20:03:19 +0900

    오크 0-1 피의분노 타게팅이 안되던 문제 수정

--- Assets/Script/Ingame/CardDropManager.cs
+++ Assets/Script/Ingame/CardDropManager.cs
@@ -493,35 +493,36 @@ public partial class CardDropManager {
                         //    unitLine[i][1].GetChild(0).Find("MagicTargetTrigger").gameObject.SetActive(false);
                         //}
                     }
-                    //else {
-                    //    if (i == targetline) {
-                    //        Debug.Log(unitLine[i][0].GetChild(0).gameObject);
-                    //        unitLine[i][0].GetChild(0).Find("ClickableUI").gameObject.SetActive(true);
-                    //        unitLine[i][0].GetChild(0).Find("MagicTargetTrigger").gameObject.SetActive(true);
-                    //    }
-                    //    else {
-                    //        if (unitLine[i][0].childCount != 0) {
-                    //            unitLine[i][0].GetChild(0).Find("ClickableUI").gameObject.SetActive(false);
-                    //            unitLine[i][0].GetChild(0).Find("MagicTargetTrigger").gameObject.SetActive(false);
-                    //        }
+                    else {
+                        if (i == targetline) {
+                            Debug.Log(unitLine[i][0].GetChild(0).gameObject);
+                            unitLine[i][0].GetChild(0).Find("ClickableUI").gameObject.SetActive(true);
+                            unitLine[i][0].GetChild(0).Find("MagicTargetTrigger").gameObject.SetActive(true);
+                        }
+                        //    else {
+                        //        if (unitLine[i][0].childCount != 0) {
+                        //            unitLine[i][0].GetChild(0).Find("ClickableUI").gameObject.SetActive(false);
+                        //            unitLine[i][0].GetChild(0).Find("MagicTargetTrigger").gameObject.SetActive(false);
+                        //        }
 
-                    //        if (unitLine[i][1].childCount != 0) {
-                    //            unitLine[i][1].GetChild(0).Find("ClickableUI").gameObject.SetActive(false);
-                    //            unitLine[i][1].GetChild(0).Find("MagicTargetTrigger").gameObject.SetActive(false);
-                    //        }
-                    //    }
+                        //        if (unitLine[i][1].childCount != 0) {
+                        //            unitLine[i][1].GetChild(0).Find("ClickableUI").gameObject.SetActive(false);
+                        //            unitLine[i][1].GetChild(0).Find("MagicTargetTrigger").gameObject.SetActive(false);
+                        //        }
+                        //    }
 
-                    //    if (enemyUnitLine[i][0].childCount != 0) {
-                    //        enemyUnitLine[i][0].GetChild(0).Find("ClickableUI").gameObject.SetActive(false);
-                    //        enemyUnitLine[i][0].GetChild(0).Find("MagicTargetTrigger").gameObject.SetActive(false);
-                    //    }
+                        //    if (enemyUnitLine[i][0].childCount != 0) {
+                        //        enemyUnitLine[i][0].GetChild(0).Find("ClickableUI").gameObject.SetActive(false);
+                        //        enemyUnitLine[i][0].GetChild(0).Find("MagicTargetTrigger").gameObject.SetActive(false);
+                        //    }
 
-                    //    if (enemyUnitLine[i][1].childCount != 0) {
-                    //        enemyUnitLine[i][1].GetChild(0).Find("ClickableUI").gameObject.SetActive(false);
-                    //        enemyUnitLine[i][1].GetChild(0).Find("MagicTargetTrigger").gameObject.SetActive(false);
-                    //    }
+                        //    if (enemyUnitLine[i][1].childCount != 0) {
+                        //        enemyUnitLine[i][1].GetChild(0).Find("ClickableUI").gameObject.SetActive(false);
+                        //        enemyUnitLine[i][1].GetChild(0).Find("MagicTargetTrigger").gameObject.SetActive(false);
+                        //    }
 
-                    //}
+                        //}
+                    }
                 }
                 return;
             }            
