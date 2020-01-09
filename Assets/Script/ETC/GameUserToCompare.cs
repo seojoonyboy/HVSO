5b7ee04811153b03c729fb06a2894fabf771694e
이종욱 <wjlee729@fbl.kr>
2020-01-09 14:44:20 +0900

    계정 선택 유저 데이터 정보 연동, 연동 오류 발생시 알림창 추가

---
+++ Assets/Script/ETC/GameUserToCompare.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Text;
+using Haegin;
+using Newtonsoft.Json;
+
+public class GameUserToCompare {
+    public int Lv { get; set; }
+    public string NickName { get; set; }
+    public int Gold { get; set; }
+    public int Crystal { get; set; }
+    public int CardCount { get; set; }
+}
+
+public class Compare {
+    public GameUserToCompare local;
+    public GameUserToCompare server;
+
+    public static Compare GetCompare(byte[] bytes) {
+        string json = Encoding.UTF8.GetString(bytes);
+        Compare value = JsonConvert.DeserializeObject<Compare>(json);
+        return value;
+    }
+}
