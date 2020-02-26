a6d13357202d8bb576c2599ed72604716e88577f
joonyboy <seojoonwon@naver.com>
2020-02-26 18:32:21 +0900

    ResourceSpreader 최적화 작업.

--- Assets/ResourceSpreader.cs
+++ Assets/ResourceSpreader.cs
@@ -8,32 +8,9 @@ public class ResourceSpreader : MonoBehaviour {
     [SerializeField] Transform targetObj;
     [SerializeField] Sprite resourceImage;
 
-    Transform startPos;
-    Transform targetPos;
+    [SerializeField] Transform poolParent, content;
 
-    private void Awake() {
-        startPos = transform.Find("Start");
-        targetPos = transform.Find("Target");
-        startPos.SetParent(startObj);
-        startPos.localPosition = Vector3.zero;
-        startPos.SetParent(transform);
-        targetPos.SetParent(targetObj);
-        targetPos.localPosition = Vector3.zero;
-        targetPos.SetParent(transform);
-
-        Transform objectList = transform.Find("Objects");
-        Vector2 imgSize = new Vector2(resourceImage.rect.width, resourceImage.rect.height);
-        for (int i = 0; i < objectList.childCount; i++) {
-            Transform obj = objectList.GetChild(i);
-            float x = Random.Range(-250, 250);
-            float y = Random.Range(-250, 250);
-            Vector2 startRandomPos = new Vector2(startObj.localPosition.x + x, startObj.localPosition.y + y);
-            obj.GetComponent<SpreadResourceController>().SetPositions(startPos.localPosition,startRandomPos, targetPos.localPosition);
-            obj.GetComponent<RectTransform>().sizeDelta = imgSize;
-            obj.GetComponent<Image>().sprite = resourceImage;
-            obj.localPosition = startPos.localPosition;
-        }
-    }
+    const int MAX_NUM = 20;
 
     public void StartSpread(int amount, Transform[] targets = null) {
         if(targets == null) StartCoroutine(SpreadResource(amount));
@@ -42,27 +19,97 @@ public class ResourceSpreader : MonoBehaviour {
                 startObj = targets[0];
                 targetObj = targets[1];
 
-                Awake();
                 StartCoroutine(SpreadResource(amount));
             }
         }
     }
 
     IEnumerator SpreadResource(int amount) {
-        Transform objectList = transform.Find("Objects");
-        if (amount > 80)
-            amount = 80;
-        for (int i = 0; i < amount * 2; i++) {
-            GameObject obj = objectList.GetChild(i).gameObject;
+        for (int i = 0; i < amount; i++) {
+            GameObject obj = GetObject();
             obj.SetActive(true);
             Vector2 start = obj.GetComponent<SpreadResourceController>().startRandomPos;
-            iTween.MoveTo(obj, iTween.Hash("x", start.x, "y", start.y, "time", 1.0f, "islocal", true));
+            iTween.MoveTo(obj, iTween.Hash(
+                    "x", start.x, 
+                    "y", start.y, 
+                    "time", 0.6f,
+                    "oncomplete", "MoveToTarget",
+                    "oncompletetarget", gameObject,
+                    "oncompleteparams", obj
+            ));
+
+            yield return new WaitForSeconds(0.02f);
         }
-        yield return new WaitForSeconds(0.2f);
-        for (int i = 0; i < amount * 2; i++) {
-            GameObject obj = objectList.GetChild(i).gameObject;
-            iTween.MoveTo(obj, iTween.Hash("x", targetPos.localPosition.x, "y", targetPos.localPosition.y, "time", 0.6f, "islocal", true));
-            yield return new WaitForSeconds(0.01f);
+    }
+
+    void MoveToTarget(GameObject obj) {
+        Vector2 end = obj.GetComponent<SpreadResourceController>().targetPos;
+        iTween.MoveTo(obj, iTween.Hash(
+                    "x", end.x,
+                    "y", end.y,
+                    "time", 0.2f,
+                    "oncomplete", "EffectFinished",
+                    "oncompletetarget", gameObject,
+                    "oncompleteparams", obj
+        ));
+    }
+
+    void EffectFinished(GameObject obj) {
+        ReturnObject(obj);
+    }
+
+    /// <summary>
+    /// Pool에서 가져오기
+    /// </summary>
+    /// <returns>가져오는 Object</returns>
+    public GameObject GetObject() {
+        GameObject obj = null;
+        if (poolParent.childCount > 1) {
+            obj = poolParent.GetChild(0).gameObject;
         }
+        else {
+            obj = Instantiate(poolParent.GetChild(0).gameObject);
+        }
+
+        obj.transform.SetParent(content);
+        
+        obj.transform.localScale = Vector3.one;
+        Vector2 imgSize = new Vector2(resourceImage.rect.width, resourceImage.rect.height);
+        obj.GetComponent<RectTransform>().sizeDelta = imgSize;
+        obj.GetComponent<Image>().sprite = resourceImage;
+        obj.GetComponent<Image>().preserveAspect = true;
+        obj.SetActive(true);
+        obj.transform.position = startObj.position;
+
+        float x = Random.Range(-250, 250);
+        float y = Random.Range(-250, 250);
+        Vector2 startRandomPos = new Vector2(startObj.position.x + x, startObj.position.y + y);
+
+        obj.GetComponent<SpreadResourceController>()
+            .SetPositions(
+                startObj.position,
+                startRandomPos,
+                targetObj.position
+            );
+
+        return obj;
+    }
+
+    /// <summary>
+    /// 반납
+    /// </summary>
+    public void ReturnObject(GameObject obj) {
+        obj.SetActive(false);
+        if (poolParent.childCount > MAX_NUM) {
+            Destroy(obj);
+        }
+        else {
+            obj.transform.SetParent(poolParent);
+            obj.transform.localPosition = Vector3.zero;
+        }
+    }
+
+    public void TriggerTestButton() {
+        StartSpread(10);
     }
 }
