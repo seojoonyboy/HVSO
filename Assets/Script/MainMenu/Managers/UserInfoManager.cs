bb75c1b66b509b970612373d57c697ee24db927e
joonyboy <seojoonwon@naver.com>
2020-03-03 17:59:14 +0900

    내 정보 화면에서 유저 경험치가 정상적으로 표기되지 않는 버그 수정.

--- Assets/Script/MainMenu/Managers/UserInfoManager.cs
+++ Assets/Script/MainMenu/Managers/UserInfoManager.cs
@@ -36,8 +36,8 @@ public class UserInfoManager : MonoBehaviour {
         Transform contents = transform.Find("InnerCanvas/Viewport/Content");
         contents.Find("PlayerInfoPanel/Info/Level/Value").GetComponent<Text>().text = AccountManager.Instance.userData.lv.ToString();
         contents.Find("PlayerInfoPanel/Info/UserName").GetComponent<TMPro.TextMeshProUGUI>().text = AccountManager.Instance.NickName;
-        contents.Find("PlayerInfoPanel/Info/LevelGauge/ValueSlider").GetComponent<Slider>().value = (float)AccountManager.Instance.userData.exp / (float)AccountManager.Instance.userData.lvExp;
-        contents.Find("PlayerInfoPanel/Info/LevelGauge/ValueText").GetComponent<TMPro.TextMeshProUGUI>().text = AccountManager.Instance.userData.exp.ToString() + "/" + AccountManager.Instance.userData.lvExp.ToString();
+        contents.Find("PlayerInfoPanel/Info/LevelGauge/ValueSlider").GetComponent<Slider>().value = (float)AccountManager.Instance.userData.lvExp / (float)AccountManager.Instance.userData.nextLvExp;
+        contents.Find("PlayerInfoPanel/Info/LevelGauge/ValueText").GetComponent<TMPro.TextMeshProUGUI>().text = AccountManager.Instance.userData.lvExp.ToString() + "/" + AccountManager.Instance.userData.nextLvExp.ToString();
         string tierName = AccountManager.Instance.scriptable_leagueData.leagueInfo.rankDetail.minorRankName;
         if (AccountManager.Instance.resource.rankIcons.ContainsKey(tierName))
             contents.Find("BattleInfoPanel/TierBannerBtn/RankIcon").GetComponent<Image>().sprite = AccountManager.Instance.resource.rankIcons[tierName];
