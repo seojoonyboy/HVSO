2e7ad1a54702bf20b004ec54db5fb84e675408d8
Yong Ju Kwak <amese1225@fbl.kr>
2020-03-14 21:26:16 +0900

    피의 분노 구현 및 비동기화성으로 callback함수 실행하도록 변경

--- Assets/Script/Ingame/ActivateCard.cs
+++ Assets/Script/Ingame/ActivateCard.cs
@@ -3,8 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
-using System.Linq;
-using System.Threading.Tasks;
 using SocketFormat;
 
 public class ActiveCard {
@@ -12,6 +10,9 @@ public class ActiveCard {
     public class CardPlayArgs {
         
     }
+
+    public delegate void SkillAction();
+    public SkillAction skillAction;
 
     public void Activate(string cardId, object args, DequeueCallback callback) {
         MethodInfo theMethod = this.GetType().GetMethod(cardId);
@@ -23,6 +24,13 @@ public class ActiveCard {
         }
         theMethod.Invoke(this, parameter);
     }
+
+    IEnumerator AfterAction(float time = 0 ,DequeueCallback callback = null) {
+        yield return new WaitForSeconds(time);
+        skillAction?.Invoke();
+        callback();
+    }
+
 
     //축복
     public void ac10006(object args, DequeueCallback callback) {
@@ -38,14 +46,18 @@ public class ActiveCard {
 
     //재배치
     public void ac10015(object args, DequeueCallback callback) {
-
         callback();
     }
 
     //피의 분노
     public void ac10016(object args, DequeueCallback callback) {
-
-        callback();
+        MagicArgs magicArgs = dataModules.JsonReader.Read<MagicArgs>(args.ToString());
+        SkillInformation info = magicArgs.skillInfo;
+        FieldUnitsObserver observer = PlayMangement.instance.UnitsObserver;
+        PlaceMonster attacker = observer.GetUnitToItemID(info.attacker).GetComponent<PlaceMonster>();
+        List<GameObject> affected = observer.GetAfftecdList(attacker.unit.ishuman, info.affected);
+        skillAction = delegate () { attacker.GetTarget(affected); };
+        AfterAction(attacker.totalAtkTime, callback).MoveNext();
     }
 
     //전쟁의 외침
