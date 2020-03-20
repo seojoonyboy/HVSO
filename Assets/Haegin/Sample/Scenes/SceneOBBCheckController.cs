c3fc397f1f3e9dec87336ed681c38204a2636be5
joonyboy <seojoonwon@naver.com>
2020-03-21 00:54:28 +0900

    OBB 다운로드 표기 오류 수정.

--- Assets/Haegin/Sample/Scenes/SceneOBBCheckController.cs
+++ Assets/Haegin/Sample/Scenes/SceneOBBCheckController.cs
@@ -364,6 +364,6 @@ public class SceneOBBCheckController : MonoBehaviour {
 
     void OnTotalProgressed(long receivedBytes, long totalSize, int count, int totalCount) {
         progressbar.Value = (int) (receivedBytes * 100 / totalSize);
-        progressText.text = (int)(receivedBytes * 100 / totalSize) + "%" + "[{count}/{totalCount}]";
+        progressText.text = (int)(receivedBytes * 100 / totalSize) + "%" + " [" + count + "/" + totalCount + "]";
     }
 }
