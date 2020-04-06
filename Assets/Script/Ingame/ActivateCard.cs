fdeb80adfd40cb9b13f9d6b78e6cf67676944f6a
Kwak Yong Ju <amese1225@fbl.kr>
2020-04-06 14:21:48 +0900

    PlaceMonster FinishAttack async -> 동기함수 비동기성 buffEffectCoroutine 주석처리, ReturnPosition bool값으로 hashTable 정리, endattack에서 bool값을 활용하는대신, activeCard쪽에서 DequeueCallback로 delegate로 재포장

--- Assets/Script/Ingame/ActivateCard.cs
+++ Assets/Script/Ingame/ActivateCard.cs
@@ -86,6 +86,13 @@ public class ActiveCard {
         List<GameObject> affected = unitObserver.GetAfftecdList(attacker.unit.ishuman, info.affected);
         EffectSystem effectSystem = EffectSystem.Instance;
         EffectSystem.ActionDelegate skillAction;
+
+        callback = delegate () {
+            for (int i = 0; i < affected.Count; i++)
+                affected[i].GetComponent<PlaceMonster>()?.CheckHP();
+            callback();
+        };
+
         skillAction = delegate () { attacker.GetTarget(affected, callback); };
         effectSystem.ShowEffectAfterCall(EffectSystem.EffectType.ANGRY, attacker.unitSpine.headbone, skillAction);
     }
@@ -562,8 +569,7 @@ public class ActiveCard {
         GameObject targetUnitObject = unitObserver.GetUnitToItemID(targetItemID);
         PlaceMonster targetUnit = targetUnitObject.GetComponent<PlaceMonster>();
         
-        targetUnit.InstanceKilled();
-        
+        targetUnit.UnitDead();        
         callback();
     }
     
