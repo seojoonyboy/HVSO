90385250bbf47ddc53339123928fade9e28452ba
joonyboy <seojoonwon@naver.com>
2020-04-12 17:17:45 +0900

    빌드 버전 수정

--- Assets/Editor/ModuleBuildScript.cs
+++ Assets/Editor/ModuleBuildScript.cs
@@ -43,7 +43,7 @@ class ModuleBuildScript
         PlayerSettings.Android.useAPKExpansionFiles = true;
         SCENES = FindBuildScenes();
 
-        PlayerSettings.bundleVersion = "20.04.10.2146";
+        PlayerSettings.bundleVersion = "20.04.12.1717";
         PlayerSettings.Android.bundleVersionCode = versionCode;
         EditorUserBuildSettings.androidBuildSubtarget = MobileTextureSubtarget.ETC2;
         PlayerSettings.Android.keystoreName = "/Volumes/Data/fbl_haegin/hvso.keystore";
@@ -62,7 +62,7 @@ class ModuleBuildScript
 
         PlayerSettings.iOS.buildNumber = versionCode.ToString();
         PlayerSettings.iOS.hideHomeButton = false;
-        PlayerSettings.bundleVersion = "20.04.10.2146";
+        PlayerSettings.bundleVersion = "20.04.12.1717";
 
         GenericBuild(SCENES, TARGET_DIR + "/XCode", BuildTarget.iOS, BuildOptions.CompressWithLz4HC);
     }
