28a718dbaed90c3c3149d62789841ec5859cce2b
이종욱 <wjlee729@fbl.kr>
2020-04-12 23:46:55 +0900

    인게임 타이머 수치 수정

--- Assets/Script/Socket/BattleConnector_receiver.cs
+++ Assets/Script/Socket/BattleConnector_receiver.cs
@@ -560,7 +560,7 @@ public partial class BattleConnector : MonoBehaviour {
         PlayerController player;
         player = PlayMangement.instance.player.isHuman ? PlayMangement.instance.player : PlayMangement.instance.enemyPlayer;
         if(ScenarioGameManagment.scenarioInstance == null && !stopTimer) {
-            player.GetComponent<IngameTimer>().RopeTimerOn(30);
+            player.GetComponent<IngameTimer>().RopeTimerOn(70);
         }
         callback();
     }
@@ -703,13 +703,13 @@ public partial class BattleConnector : MonoBehaviour {
         //human 실드 발동
         if (camp == "human") {
             if (!isHuman) {
-                PlayMangement.instance.enemyPlayer.GetComponent<IngameTimer>()?.PauseTimer(20);
+                PlayMangement.instance.enemyPlayer.GetComponent<IngameTimer>()?.PauseTimer(25);
             }           
         }
         //orc 실드 발동
         else {
             if (isHuman) {
-                PlayMangement.instance.enemyPlayer.GetComponent<IngameTimer>()?.PauseTimer(20);
+                PlayMangement.instance.enemyPlayer.GetComponent<IngameTimer>()?.PauseTimer(25);
             }
         }
 
