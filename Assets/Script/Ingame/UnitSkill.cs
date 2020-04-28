8bf521aaf6ef8ffc6f260b3067c1c1fea269c10a
Kwak Yong Ju <amese1225@fbl.kr>
2020-04-28 15:37:15 +0900

    녹색의 현자 수정

--- Assets/Script/Ingame/UnitSkill.cs
+++ Assets/Script/Ingame/UnitSkill.cs
@@ -185,12 +185,21 @@ public class UnitSkill {
 
         GameObject unit = unitObserver.GetUnitToItemID(from);
         PlaceMonster unitData = unit.GetComponent<PlaceMonster>();
-        
-        if(unitData.isPlayer == true) 
-            PlayMangement.instance.player.resource.Value = PlayMangement.instance.socketHandler.gameState.players.myPlayer(PlayMangement.instance.player.isHuman).resource;        
-        else 
+
+        if (unitData.isPlayer == true) {
+            PlayMangement.instance.player.resource.Value = PlayMangement.instance.socketHandler.gameState.players.myPlayer(PlayMangement.instance.player.isHuman).resource;
+
+            if(PlayMangement.instance.player.isHuman == true) 
+                PlayMangement.instance.player.ActivePlayer();            
+            else 
+                PlayMangement.instance.player.ActiveOrcTurn();        
+        }
+        else
             PlayMangement.instance.enemyPlayer.resource.Value = PlayMangement.instance.socketHandler.gameState.players.enemyPlayer(PlayMangement.instance.enemyPlayer.isHuman).resource;
         
+
+
+
         callback();
     }
 
