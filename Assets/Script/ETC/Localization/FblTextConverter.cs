379d61df72c74ce773f2feda56d9f8145d893766
KHG <cow3323@fbl.kr>
2020-05-05 16:53:25 +0900

    NotoSans CJK 폰트 적용중

--- Assets/Script/ETC/Localization/FblTextConverter.cs
+++ Assets/Script/ETC/Localization/FblTextConverter.cs
@@ -48,11 +48,39 @@ public class FblTextConverter : MonoBehaviour {
     }
 
     public virtual void RefreshText() {
-        if (string.IsNullOrEmpty(category) || string.IsNullOrEmpty(key)) return;
 
         AccountManager accountManager = AccountManager.Instance;
         ResourceManager resourceManager = accountManager.resource;
-        var languageSetting = accountManager.GetLanguageSetting();        
+        var languageSetting = accountManager.GetLanguageSetting();
+
+        switch (type) {
+            case TextType.TEXTMESHPROUGUI:
+                try {
+                    var tmProComp = GetComponent<TextMeshProUGUI>();
+
+                    if (tmProComp.font.name.Contains("Regular")) tmProComp.font = resourceManager.tmp_fonts[languageSetting + "_Regular"];
+                    else if (tmProComp.font.name.Contains("Bold")) tmProComp.font = resourceManager.tmp_fonts[languageSetting + "_Bold"];
+                }
+                catch (Exception ex) {
+                    Logger.Log("TextMeshProUGUI 컴포넌트를 찾을 수 없습니다. \n 대상 : " + transform.parent.name);
+                }
+                break;
+            case TextType.UGUITEXT:
+                try {
+                    var textComp = GetComponent<Text>();
+                    if (textComp.font.name.Contains("Regular")) textComp.font = resourceManager.fonts[languageSetting + "_Regular"];
+                    else if (textComp.font.name.Contains("Bold")) textComp.font = resourceManager.fonts[languageSetting + "_Bold"];
+                }
+                catch (Exception ex) {
+                    Logger.Log("Text 컴포넌트를 찾을 수 없습니다. \n 대상 : " + transform.parent.name);
+                }
+                break;
+
+            case TextType.UGUIIMAGE:
+                break;
+        }
+
+        if (string.IsNullOrEmpty(category) || string.IsNullOrEmpty(key)) return;
 
         switch (type) {
             case TextType.TEXTMESHPROUGUI:
@@ -62,9 +90,7 @@ public class FblTextConverter : MonoBehaviour {
                     result = result.Replace("\\n", "\n");
 
                     var tmProComp = GetComponent<TextMeshProUGUI>();
-                    if (tmProComp.font.name.Contains("Regular")) tmProComp.font = resourceManager.tmp_fonts[languageSetting + "_Regular"];
-                    else if (tmProComp.font.name.Contains("Bold")) tmProComp.font = resourceManager.tmp_fonts[languageSetting + "_Bold"];
-                    
+
                     tmProComp.text = result;
                     basicText = result;
                 }
@@ -79,8 +105,6 @@ public class FblTextConverter : MonoBehaviour {
                     result = result.Replace("\\n", "\n");
 
                     var textComp = GetComponent<Text>();
-                    if (textComp.font.name.Contains("Regular")) textComp.font = resourceManager.fonts[languageSetting + "_Regular"];
-                    else if (textComp.font.name.Contains("Bold")) textComp.font = resourceManager.fonts[languageSetting + "_Bold"];
 
                     textComp.text = result;
                     basicText = result;
