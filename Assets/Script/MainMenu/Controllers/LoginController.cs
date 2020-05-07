f6cc4809be618aece33342b12ce9e815948c3baf
joonyboy <seojoonwon@naver.com>
2020-05-07 19:18:30 +0900

    로그인 화면에서 Tab The Screen 디테일 수정

--- Assets/Script/MainMenu/Controllers/LoginController.cs
+++ Assets/Script/MainMenu/Controllers/LoginController.cs
@@ -95,7 +95,7 @@ public class LoginController : MonoBehaviour {
         isClicked = false;
         IAPSetup.Instance.Init();
         
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(3.0f);
         logo.SetActive(true);
         
         //CustomVibrate.Vibrate(1000);
