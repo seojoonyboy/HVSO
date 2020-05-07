aa193b003d93a4c9234b645caf4ca1d35ab90c5d
Kwak Yong Ju <amese1225@fbl.kr>
2020-05-07 17:53:34 +0900

    스턴 이펙트 배틀턴 종료시 정상적으로 사라지도록 변경

--- Assets/Script/Ingame/AboutSkill/SkillComp/0611Renewal/Attributes/stun.cs
+++ Assets/Script/Ingame/AboutSkill/SkillComp/0611Renewal/Attributes/stun.cs
@@ -1,15 +1,25 @@
 using TMPro;
 using UnityEngine;
+using System.Collections.Generic;
+using UniRx;
 
 namespace SkillModules {
     public class stun : UnitAttribute {
         private TextMeshPro textPro;
         private void Start() {
             EffectSystem.Instance.ContinueEffect(EffectSystem.EffectType.STUN, gameObject.GetComponent<PlaceMonster>().unitSpine.headbone);
+            //OnEvent onEvent = PlayMangement.instance.OnBattleTurnEnd;
+            //Observable.FromEvent<OnEvent>(h => () => h(onEvent) , h => onEvent += h, h => onEvent -= h).First().Subscribe(_ => { stunRemove();}).AddTo(GetComponent<stun>());
+            PlayMangement.instance.EventHandler.AddListener(IngameEventHandler.EVENT_TYPE.END_BATTLE_TURN, this.stunRemove);
         }
 
-        void OnDestroy() {
+        void stunRemove(System.Enum event_type, Component Sender, object Param) {
             EffectSystem.Instance.DisableEffect(EffectSystem.EffectType.STUN, gameObject.GetComponent<PlaceMonster>().unitSpine.headbone);
+            Destroy(gameObject.GetComponent<stun>());
+        }
+
+        private void OnDestroy() {
+            PlayMangement.instance.EventHandler.RemoveListener(IngameEventHandler.EVENT_TYPE.END_BATTLE_TURN, this.stunRemove);
         }
     }
 }
