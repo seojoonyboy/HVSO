ca15550f92a61f0735861547437782c28c6ed633
joonyboy <seojoonwon@naver.com>
2020-05-13 15:33:29 +0900

    릴리즈 버전 정보 업데이트

--- Assets/Editor/ModuleBuildScript.cs
+++ Assets/Editor/ModuleBuildScript.cs
@@ -43,7 +43,7 @@ class ModuleBuildScript
         PlayerSettings.Android.useAPKExpansionFiles = true;
         SCENES = FindBuildScenes();
 
-        PlayerSettings.bundleVersion = "20.05.08.1715";
+        PlayerSettings.bundleVersion = "20.05.13.1533";
         PlayerSettings.Android.bundleVersionCode = versionCode;
         EditorUserBuildSettings.androidBuildSubtarget = MobileTextureSubtarget.ETC2;
         PlayerSettings.Android.keystoreName = "/Volumes/Data/fbl_haegin/hvso.keystore";
@@ -62,7 +62,7 @@ class ModuleBuildScript
 
         PlayerSettings.iOS.buildNumber = versionCode.ToString();
         PlayerSettings.iOS.hideHomeButton = false;
-        PlayerSettings.bundleVersion = "20.05.08.1715";
+        PlayerSettings.bundleVersion = "20.05.13.1533";
 
         GenericBuild(SCENES, TARGET_DIR + "/XCode", BuildTarget.iOS, BuildOptions.CompressWithLz4HC);
     }
