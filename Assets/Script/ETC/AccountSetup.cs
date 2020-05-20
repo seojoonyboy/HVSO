7fe08ffbb34f0875cfb2740e6cefe7cd2ac43027
이종욱 <wjlee729@fbl.kr>
2020-05-20 20:12:19 +0900

    시스템 업적 보기만 추가

--- Assets/Script/ETC/AccountSetup.cs
+++ Assets/Script/ETC/AccountSetup.cs
@@ -20,36 +20,13 @@ using TMPro;
     
     public Sprite btnEnable;
     public Sprite btnDisable;
-    public GameObject systemDialog;
-    public GameObject eulaText;
     public GameObject hideModal;
     public Canvas canvas;
     public AccountDialogController accountDialog;
 
     public void Init() {
-        WebClientInit();
         ButtonInit();
-        AccountManager.Instance.tokenSetFinished += delegate{};
-    }
-
-    public void Destory() {
-        DestoryWebClient();
-        AccountManager.Instance.tokenSetFinished = null;
-    }
-
-    private void WebClientInit() {
         webClient = WebClient.GetInstance();
-
-        webClient.ErrorOccurred += OnErrorOccurred;
-        webClient.Processing += OnProcessing;
-        webClient.RetryOccurred += RetryOccurred;
-        webClient.RetryFailed += RetryFailed;
-        webClient.MaintenanceStarted += OnMaintenanceStarted;
-        webClient.Logged += (string log) => {
-#if MDEBUG
-            Debug.Log("Unity   " + log);
-#endif
-        };
     }
 
     public void ButtonInit() {
@@ -107,57 +84,6 @@ using TMPro;
             appleBtn.enabled = false;
             //appleBtn.image.sprite = btnDisable;
         }
-#endif
-    }
-
-    private void DestoryWebClient() {
-        if (webClient != null) {
-            webClient.ErrorOccurred -= OnErrorOccurred;
-            webClient.Processing -= OnProcessing;
-            webClient.RetryOccurred -= RetryOccurred;
-            webClient.RetryFailed -= RetryFailed;
-            webClient.MaintenanceStarted -= OnMaintenanceStarted;
-        }
-    }
-
-    void OnProcessing(ReqAndRes rar){
-        if(ProtocolId.IssueJWT == rar.Res.ProtocolId) {
-            IssueJWTRes result = (IssueJWTRes)rar.Res;
-            AccountManager.Instance.TokenId = result.Token;
-            Modal.instantiate("계정 연동이 완료 되었습니다.", Modal.Type.CHECK, () => FBL_SceneManager.Instance.LoadScene(FBL_SceneManager.Scene.MAIN_SCENE));
-        }
-    }
-
-    public void OnErrorOccurred(int error) {
-        OnNetworkError();
-    }
-
-    void OnNetworkError() {
-        UGUICommon.ShowMessageDialog(systemDialog, eulaText, canvas, TextManager.GetString(TextManager.StringTag.NetworkError), TextManager.GetString(TextManager.StringTag.NetworkErrorMessage), (UGUICommon.ButtonType buttonType) => {
-            if (buttonType == UGUICommon.ButtonType.Ok) {
-                ThreadSafeDispatcher.ApplicationQuit();
-            }
-        });
-    }
-
-    void OnMaintenanceStarted() {
-        // 메인터넌스가 시작되었다.
-        UGUICommon.ShowMessageDialog(systemDialog, eulaText, canvas, TextManager.GetString(TextManager.StringTag.ServerMaintenanceTitle), TextManager.GetString(TextManager.StringTag.ServerMaintenance), (UGUICommon.ButtonType buttonType) =>
-        {
-            if (buttonType == UGUICommon.ButtonType.Ok)
-            {
-                ThreadSafeDispatcher.ApplicationQuit();
-            }
-        });
-    }
-    
-    void RetryFailed(Protocol protocol) {
-        OnNetworkError();
-    }
-
-    void RetryOccurred(Protocol protocol, int retryCount) {
-#if MDEBUG
-        Debug.Log("Retry Occurred  " + retryCount);
 #endif
     }
 
