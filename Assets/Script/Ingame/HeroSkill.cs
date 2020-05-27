b74d97f3e06ac60eb3ca171ffec95e48f6a1bb27
이종욱 <wjlee729@fbl.kr>
2020-05-27 14:19:53 +0900

    영웅 스킬 콜백 누락 수정

--- Assets/Script/Ingame/HeroSkill.cs
+++ Assets/Script/Ingame/HeroSkill.cs
@@ -7,7 +7,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 public class HeroSkill {
-    public void Activate(bool isPlayer, string heroId, List<JToken> toList, string trigger) {
-        
+    public void Activate(bool isPlayer, string heroId, List<JToken> toList, string trigger, DequeueCallback callback) {
+        callback();
     }
 }
