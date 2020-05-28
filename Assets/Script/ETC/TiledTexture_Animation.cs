6bcb64ad129027621c2ad041e03435f7b51f1cb0
joonyboy <seojoonwon@naver.com>
2020-05-28 19:30:51 +0900

    메인화면 패턴 애니메이션 추가

---
+++ Assets/Script/ETC/TiledTexture_Animation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TiledTexture_Animation : MonoBehaviour {
+    float scrollSpeed = 0.2f;
+    private RawImage _rawImage;
+    private float uvHeight, uvWidth;
+    private void Start() {
+        _rawImage = GetComponent<RawImage>();
+        
+        var uvRect = _rawImage.uvRect;
+        uvHeight = uvRect.height;
+        uvWidth = uvRect.width;
+        
+        StartCoroutine(UpdateTexture());
+    }
+
+    IEnumerator UpdateTexture() {
+        while (true) {
+            float offset = -1 * Time.time * scrollSpeed;
+            _rawImage.uvRect = new Rect(offset, 0, uvWidth, uvHeight);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
