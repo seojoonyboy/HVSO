b99c7a0cf629998327d3a6a48e7e2515af8e6f80
Kwak Yong Ju <amese1225@fbl.kr>
2020-06-03 17:26:20 +0900

    맨티스 481 공격력 및 방어력 수치가 증가하였지만, 초록색으로 수치가 표기되지 않는 현상 : granted와 hp 기준 검사 수정

--- Assets/Script/PlaceMonster.cs
+++ Assets/Script/PlaceMonster.cs
@@ -837,25 +837,30 @@ public class PlaceMonster : MonoBehaviour {
         int grantedHP = 0;
         int grantedAtk = 0;
         if (granted != null && granted.Length > 0) { foreach (Granted grant in granted) { if (grant.hp != 0) grantedHP += grant.hp; if (grant.attack != 0) grantedAtk += grant.attack; } };
-        int hpPos = unit.hp.Value + grantedHP;
+
+        int originalHP = AccountManager.Instance.allCardsDic[unit.cardId].hp.Value;
+
+        int hpPos = originalHP + grantedHP;
         int atkPos = unit.originalAttack + grantedAtk;
 
 
-        if (unit.currentHp < hpPos)
-            hpText.color = Color.red;
-        else if (unit.currentHp > hpPos)
-            hpText.color = Color.green;
-        else
+        if (grantedHP == 0 && unit.currentHp == hpPos)
             hpText.color = Color.white;
+        else if (unit.currentHp < hpPos)
+            hpText.color = Color.red;
+        else
+            hpText.color = Color.green;
+
 
         atkText.text = unit.attack.ToString();
 
-        if (unit.attack < atkPos)
-            atkText.color = Color.red;
-        else if (unit.attack > atkPos)
-            atkText.color = Color.green;
-        else
+
+        if (grantedAtk == 0 && unit.attack == atkPos)
             atkText.color = Color.white;
+        else if (unit.attack < atkPos)
+            atkText.color = Color.red;
+        else
+            atkText.color = Color.green;
     }
 
     private void ReturnPosition(bool isAttacker = false) {
